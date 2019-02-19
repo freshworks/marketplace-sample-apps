ae306a78db700f4c90a885dbc2f460c01a658d32
Saif Shines <saifshine7@gmail.com>
2019-02-19 14:42:27 +0530

    [your-sample-app] refactored: submission #1

--- Freshdesk/v2/your_first_app/app/app.js
+++ Freshdesk/v2/your_first_app/app/app.js
@@ -1,16 +1,26 @@
-$(document).ready( function() {
-    app.initialized()
-        .then(function(_client) {
-          var client = _client;
-          client.events.on('app.activated',
-            function() {
-                client.data.get('contact')
-                    .then(function(data) {
-                        $('#apptext').text("Ticket created by " + data.contact.name);
-                    })
-                    .catch(function(e) {
-                        console.log('Exception - ', e);
-                    });
-        });
-    });
+
+/**
+ * @desc - Sample App shows fectching Contact from Data API and shows on Ticket
+ * sidebar
+ */
+
+$(document).ready(() => {
+  app.initialized().then(function(_client) { // Client Object is obtained
+    const client = _client;
+    /**
+     * app.activated() is bought into scope, and timing differs
+     * based on the location of the app in the FreshProduct.
+     */
+    client.events.on('app.activated', () => {
+      /**
+       * Making an Asyc call to Data API and feching the details from
+       * the sample Payload.
+       */
+      client.data.get('contact').then(function(data) {
+        $('#apptext').text("Ticket created by "+ data.contact.name);
+      }).catch((e) => { 
+        console.log('Exception -',e);
+      }); 
+   });
+  });
 });
