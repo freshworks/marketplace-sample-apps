20ebc6c4bac2478998476542f06df5588d8e759b
Saif Shines <saifshine7@gmail.com>
2019-02-23 22:57:10 +0530

    [crm app][No Test Run done: Unable to figure out which username & password in iparams local test]

--- Freshdesk/v2/sample_crm_app/app/app.js
+++ Freshdesk/v2/sample_crm_app/app/app.js
@@ -1,36 +1,53 @@
-var BASE_URL = 'https://<%= iparam.subdomain %>.freshsales.io';
-var AUTHORIZATION_TEMPLATE = "Basic <%= encode(iparam.username + ':' + iparam.password)%>";
-var CONTACT_INFO_MAPPING = {
+/**
+ * @description - Everytime the ticket details page is loaded, this app makes an
+ * API call to the Freshsales CRM to retrieve additional information about
+ * the ticket requester.
+ * 
+ * 1. Using data API to get ticket requester's details
+ * 2. Request API - GET request to retrieve the ticket requester's details.
+ */
+
+const BASE_URL = 'https://<%= iparam.subdomain %>.freshsales.io';
+const AUTHORIZATION_TEMPLATE = "Basic <%= encode(iparam.username + ':' + iparam.password)%>";
+const CONTACT_INFO_MAPPING = {
   display_name: 'Name',
   email: 'Email',
   mobile_number: 'Mobile'
 };
-var WORK_INFO_MAPPING = {
+const WORK_INFO_MAPPING = {
   job_title: 'Job Title',
   department: 'Department'
 };
 
+/**@fires - Interface API to show Notification */
 function displayErr(message) {
   client.interface.trigger('showNotify', { type: 'danger', message: message});
 }
 
+/**@fires - A promise
+ * To search for an Contact in the JSON endpoint
+ * @param - Stirng, email
+ */
 function searchContact(email) {
-  return new Promise(function(resolve) {
+  return new Promise((resolve) => {
     client.request.get(BASE_URL + '/api/search?q=' + email + '&include=contact', {
       headers: {
         Authorization: AUTHORIZATION_TEMPLATE
       }
-    })
-    .then(function(data) {
+    }).then((data) => {
       resolve(JSON.parse(data.response));
-    }, function() {
+    }, 
+    () => {
       displayErr('Error searching CRM database');
     });
   });
 }
 
+/**@fires - A Reqeust API using a Promise, which fetches contact details from
+ * the contactId  
+ */
 function fetchContactDetails(contactId) {
-  return new Promise(function(resolve) {
+  return new Promise((resolve) => {
     client.request.get(BASE_URL + '/api/contacts/' + contactId, {
       headers: {
         Authorization: AUTHORIZATION_TEMPLATE
@@ -46,10 +63,9 @@ function fetchContactDetails(contactId) {
 
 function getTicketContact() {
   return new Promise(function(resolve) {
-    client.data.get('contact')
-    .then(function(data) {
+    client.data.get('contact').then((data) => {
       resolve(data);
-    }, function() {
+    }, () => {
       displayErr('Error fetching contact from CRM database');
     });
   });
@@ -69,33 +85,30 @@ function displayInfo(title, data) {
   }
 }
 
-$(document).ready( function() {
-  async.waterfall([
-    function(callback) {
-      app.initialized()
-      .then(function(_client) {
+$(document).ready( () => {
+  async.waterfall([(callback) => {
+      app.initialized().then((_client) => {
         window.client = _client;
         callback();
       });
     },
 
-    function(callback) {
+    (callback) => {
       // Get the contact information through data API
-      getTicketContact()
-      .then(function(contactInformation) {
+      getTicketContact().then((contactInformation) => {
         callback(null, contactInformation);
       });
     },
 
-    function(contactInformation, callback) {
+    (contactInformation, callback) => {
       // Search CRM
       searchContact(contactInformation.contact.email)
-      .then(function(searchResult) {
+      .then((searchResult) => {
         callback(null, searchResult);
       });
     },
 
-    function(searchResult, callback) {
+    (searchResult, callback) => {
       if (searchResult.length > 0) {
         // Pull information from CRM
         fetchContactDetails(searchResult[0].id)
@@ -108,7 +121,7 @@ $(document).ready( function() {
       }
     },
 
-    function(crmContactInformation) {
+    (crmContactInformation) => {
       for (var contactKey in CONTACT_INFO_MAPPING) {
         displayInfo(CONTACT_INFO_MAPPING[contactKey], crmContactInformation.contact[contactKey]);
       }
