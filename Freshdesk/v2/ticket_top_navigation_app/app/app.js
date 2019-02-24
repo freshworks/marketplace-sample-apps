71b413bdfaf186a108c971123a26781e2c9212bf
Saif Shines <saifshine7@gmail.com>
2019-02-24 17:36:04 +0530

    [Ticket top navigation app] - Refactored

--- Freshdesk/v2/ticket_top_navigation_app/app/app.js
+++ Freshdesk/v2/ticket_top_navigation_app/app/app.js
@@ -1,24 +1,38 @@
-$(document).ready( function() {
+/** This app adds a shortcut to start the timer in the top navigation of the
+ * tickt details page. On clicking the app icon, the Start Timer form will be
+ * shown.
+ */
+
+$(document).ready( () => {
   // Initialize channel
-  app.initialized().then(function(_client) {
-    var client = _client;
+  app.initialized().then((_client) => {
+    let client = _client;
     // App activate callback
-    client.events.on('app.activated', function() {
+    client.events.on('app.activated', () => {
       // Open modal to collect params to start timer Interface API
-      client.interface.trigger('showModal', { title: 'Start timer', template: 'startTimer.html' })
-      .then(null, function(error) {
-        client.interface.trigger('showNotify', { type: 'error', message: 'Some error has occured in \'Start timer app\'.'});
+      client.interface.trigger('showModal', { 
+        title: 'Start timer', 
+        template: 'startTimer.html' })
+      .then(null, (error) => {
+        client.interface.trigger('showNotify', { 
+          type: 'error', 
+          message: 'Some error has occured in \'Start timer app\'.'});
       });
     });
 
-    client.instance.receive(function(event) {
+    client.instance.receive((event) => {
       let data = event.helper.getData();
       if (data.message.agent) {
         // start the timer
-        client.interface.trigger('start', { id: 'timer', value: data.message });
+        client.interface.trigger('start', { 
+          id: 'timer', 
+          value: data.message 
+        });
       }
     });
-  }, function(error) {
-    client.interface.trigger('showNotify', { type: 'error', message: 'Some error has occured in \'Start timer app\'.'});
+  }, (error) => {
+    client.interface.trigger('showNotify', { 
+      type: 'error', 
+      message: 'Some error has occured in \'Start timer app\'.'});
   });
 });
