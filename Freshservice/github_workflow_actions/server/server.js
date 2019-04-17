51d6ef4c39088d9d16a51958bc6ee188030e4f17
Raviraj Subramanian <ravirajsubramanian@gmail.com>
2019-04-17 15:36:31 +0530

    changing request package to &request for http calls

--- Freshservice/github_workflow_actions/server/server.js
+++ Freshservice/github_workflow_actions/server/server.js
@@ -1,47 +1,36 @@
-'use strict';
-
-const request = require('request');
+'use strict'
 
 exports = {
 
-  inviteUser : function(args) {
+  inviteUser: function (args) {
+    const url = `https://api.github.com/orgs/${args.iparams.GitHubOrganization}/memberships/${args.user_github_handle}`
     const options = {
-      url : `https://api.github.com/orgs/${args.iparams.organisation}/memberships/${args.user_github_handle}`,
-      headers : {
-        Authorization : `Bearer ${args.iparams.gitAdminKey}`,
-        'Content-Type': 'application/json'
+      headers: {
+        Authorization: `Bearer ${args.iparams.GitHubApiKey}`,
+        'User-Agent': 'Awesome-Octocat-App' // This is required by the  GitHub API
       },
-      method: 'PUT',
-      data: {
-        'role': args.user_role || 'member'
-      }
-    };
-
-    request(options, function(error, response) {
-      if(!error && response.statusCode === 200) {
-        renderData(null, {'success': true, 'data': response.body});
-      }
-
-      renderData(null,{'success': false, 'error': 'Error while sending request to user'});
-    });
+      // sending the request body as value in json key will automatically append the request type header
+      json: { role: args.user_role || 'member' }
+    }
+    $request.put(url, options)
+      .then(
+        data => { renderData(null, { success: true, data: data.response }) },
+        () => { renderData(null, { success: false, error: 'Failed to invite the user to the GitHub organization' }) }
+      )
   },
 
-  deleteUser: function(args) {
+  deleteUser: function (args) {
+    const url = `https://api.github.com/orgs/${args.iparams.GitHubOrganization}/memberships/${args.user_github_handle}`
     const options = {
-      url : `https://api.github.com/orgs/${args.iparams.organisation}/memberships/${args.user_github_handle}`,
-      headers : {
-        Authorization : `Bearer ${args.iparams.gitAdminKey}`,
-        'Content-Type': 'application/json'
-      },
-      method: 'DELETE'
-    };
-
-    request(options, function(error, response) {
-      if(!error && response.statusCode === 204) {
-        renderData(null, {'success': true, 'data': response});
+      headers: {
+        Authorization: `Bearer ${args.iparams.GitHubApiKey}`,
+        'User-Agent': 'Awesome-Octocat-App'
       }
-
-      renderData(null,{'success': false, 'error': response.body});
-    });
+    }
+    $request.delete(url, options)
+      .then(
+        data => { renderData(null, { success: true, data: data }) },
+        () => { renderData(null, { success: false, error: response.body }) }
+      )
   }
-};
+}
