7147421ca66704cf8781b1f3963753f742b36e1d
Raviraj Subramanian <ravirajsubramanian@gmail.com>
2019-04-22 15:36:57 +0530

    minor fixes on formatting

--- Freshservice/github_workflow_actions/server/server.js
+++ Freshservice/github_workflow_actions/server/server.js
@@ -3,11 +3,11 @@
 exports = {
 
   inviteUser: function (args) {
-    const url = `https://api.github.com/orgs/${args.iparams.GitHubOrganization}/memberships/${args.user_github_handle}`
+    const url = `https://api.github.com/orgs/${args.iparams.github_organization}/memberships/${args.user_github_handle}`
     const options = {
       headers: {
-        Authorization: `Bearer ${args.iparams.GitHubApiKey}`,
-        'User-Agent': 'Awesome-Octocat-App' // This is required by the  GitHub API
+        Authorization: `Bearer ${args.iparams.github_api_key}`,
+        'User-Agent': 'Awesome-Octocat-App' // This is required by the GitHub API
       },
       // sending the request body as value in json key will automatically append the request type header
       json: { role: args.user_role || 'member' }
@@ -15,22 +15,28 @@ exports = {
     $request.put(url, options)
       .then(
         data => { renderData(null, { success: true, data: data.response }) },
-        () => { renderData(null, { success: false, error: 'Failed to invite the user to the GitHub organization' }) }
+        () => {
+          console.log('Failed to invite the user to the GitHub organization')
+          renderData(null, { success: false, error: 'Failed to invite the user to the GitHub organization' })
+        }
       )
   },
 
   deleteUser: function (args) {
-    const url = `https://api.github.com/orgs/${args.iparams.GitHubOrganization}/memberships/${args.user_github_handle}`
+    const url = `https://api.github.com/orgs/${args.iparams.github_organization}/memberships/${args.user_github_handle}`
     const options = {
       headers: {
-        Authorization: `Bearer ${args.iparams.GitHubApiKey}`,
+        Authorization: `Bearer ${args.iparams.github_api_key}`,
         'User-Agent': 'Awesome-Octocat-App'
       }
     }
     $request.delete(url, options)
       .then(
         data => { renderData(null, { success: true, data: data }) },
-        () => { renderData(null, { success: false, error: response.body }) }
+        () => {
+          console.log('Failed to invite the user to the GitHub organization')
+          renderData(null, { success: false, error: response.body })
+        }
       )
   }
 }
