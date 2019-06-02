9f8c146c2893aba4d2e4f974d3a399db67597504
Saif Shines <saifshine7@gmail.com>
2019-06-02 22:45:16 +0530

    Your First App - Phase 2 Complete

--- Freshdesk/v2/your_first_app/app/app.js
+++ Freshdesk/v2/your_first_app/app/app.js
@@ -4,25 +4,23 @@
  */
 
 $(document).ready(function(){   
-  app.initialized().then((_client)=>{
-    window.client = _client;
-    client.events.on('app.activated',()=>{
-    console.log(`App activated`);
-    getcontactdata();
+  (function(){
+    app.initialized().then((_client)=>{
+      window.client = _client;
+      client.events.on('app.activated',()=>{
+      getcontactdata();
+      });
     });
-  });
-
-  function getcontactdata(){
-    console.log(`getting app data`);
-    client.data.get('contact').then((data)=>{
-    $('#apptext').text(`Ticket created by ${data.contact.name}`);  
-    congratulations();
-    },(e)=>{
-      console.log('Exeception -',e);
-    });
-  }
-
-  function congratulations(){
-    $('.content').text(`Congratulations on creating your first app!`);
-  }
+    function getcontactdata(){
+      client.data.get('contact').then((data)=>{
+      $('#apptext').text(`Ticket created by ${data.contact.name}`);  
+      congratulations();
+      },(e)=>{
+        console.log(`Found some error. More info: ${e}`);
+      });
+    }
+    function congratulations(){
+      $('.content').text(`Congratulations on creating your first app`);
+    }
+  })();
 });
