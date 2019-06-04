0a0575c92a06120d41cefb2e00fbaf047a69d5a5
Saif Shines <saifshine7@gmail.com>
2019-06-04 09:14:04 +0530

    simple api app - Phase 2 complete

--- Freshdesk/v2/simple_request_app/app/app.js
+++ Freshdesk/v2/simple_request_app/app/app.js
@@ -1,40 +1,89 @@
+// /**
+//  * @description - This app makes REST API calls to httpbin.org.
+//  * 
+//  * 1. Request API - GET, POST, PUT & DELETE
+//  */
+
+// const BASE_URL = "https://httpbin.org/";
+
+// function displayStatus(type, message) {
+//   client.interface.trigger('showNotify', {
+//      type: type,
+//      message: message
+//     });
+// }
+
+// $(document).ready( function() {
+//   app.initialized().then((_client) => {
+//     window.client = _client;
+//     client.events.on('app.activated', initButton);
+    
+//     function initButton() {
+//       jQuery(".request-btn").click((e)=>{
+//       var method = e.target.getAttribute('method');
+//       var url = BASE_URL + method;
+//       var options = {
+//         "headers" : {
+//           "Content-Type": "application/json"
+//         }
+//       };
+//       client.request[method](url, options)
+//       .then(() => {
+//         displayStatus('success', method.toUpperCase() + ' request successful.');
+//       }, () => {
+//         displayStatus('danger', method.toUpperCase() + ' request failed.');
+//       });
+//       });
+//     }
+
+//   });
+// });
+
 /**
  * @description - This app makes REST API calls to httpbin.org.
  * 
  * 1. Request API - GET, POST, PUT & DELETE
  */
 
-const BASE_URL = "https://httpbin.org/";
+const BASE_URL = "https://httpbin.org/"
 
-function displayStatus(type, message) {
-  client.interface.trigger('showNotify', {
-     type: type,
-     message: message
+
+ $(document).ready(function(){
+  (function(){
+    app.initialized().then((_client)=>{
+      window.client = _client;
+      client.events.on('app.activated', init);
     });
-}
-
-$(document).ready( function() {
-  app.initialized().then((_client) => {
-    window.client = _client;
-    client.events.on('app.activated', initButton);
     
-    function initButton() {
-      jQuery(".request-btn").click((e)=>{
-      var method = e.target.getAttribute('method');
-      var url = BASE_URL + method;
-      var options = {
+    function init(){
+      $(".request-btn").click((e)=>{
+        let method = e.target.getAttribute('method');
+        const url = BASE_URL + method;
+        const options = {
         "headers" : {
           "Content-Type": "application/json"
         }
-      };
-      client.request[method](url, options)
-      .then(() => {
-        displayStatus('success', method.toUpperCase() + ' request successful.');
-      }, () => {
-        displayStatus('danger', method.toUpperCase() + ' request failed.');
-      });
+        };
+        console.log(`${e}`);
+        req(method,url,options);
       });
     }
 
-  });
-});
+    function req(method,url,options){
+      client.request[method](url, options)
+      .then(() => {
+        displayStatus('success',`${method.toUpperCase()} request successful`);
+      }, () => {
+        displayStatus('danger', `${method.toUpperCase()} request failed`);
+      });
+    }
+
+    function displayStatus(type, message){
+      client.interface.trigger('showNotify',{
+        type: type,
+        message: message
+      });
+    }
+  
+  })();
+ });
