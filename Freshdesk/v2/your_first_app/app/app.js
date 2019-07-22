02c09b90ba5ddf54a23312d78a236750042be964
Saif Ali Shaik <saifshine7@gmail.com>
2019-07-22 14:53:13 +0530

    Remove window and closures

--- Freshdesk/v2/your_first_app/app/app.js
+++ Freshdesk/v2/your_first_app/app/app.js
@@ -3,24 +3,24 @@
  * sidebar
  */
 
-$(document).ready(function(){   
-  (function(){
-    app.initialized().then((_client)=>{
-      window.client = _client;
-      client.events.on('app.activated',()=>{
-      getcontactdata();
-      });
+$(document).ready(function () {
+  app.initialized().then((_client) => {
+    client = _client;
+    client.events.on('app.activated', () => {
+      getcontactdata(client);
     });
-    function getcontactdata(){
-      client.data.get('contact').then((data)=>{
-      $('#apptext').text(`Ticket created by ${data.contact.name}`);  
+  });
+
+  function getcontactdata(client) {
+    client.data.get('contact').then((data) => {
+      $('#apptext').text(`Ticket created by ${data.contact.name}`);
       congratulations();
-      },(e)=>{
-        console.log(`Found some error. More info: ${e}`);
-      });
-    }
-    function congratulations(){
-      $('.content').text(`Congratulations on creating your first app`);
-    }
-  })();
+    }, (e) => {
+      console.log('Found some error. More info:', e);
+    });
+  }
+
+  function congratulations() {
+    $('.content').text(`Congratulations on creating your first app`);
+  }
 });
