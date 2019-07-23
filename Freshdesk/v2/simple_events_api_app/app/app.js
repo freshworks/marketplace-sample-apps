c5b8e138d3d8987f5bb1e29e0f99ff535e16a6ea
Saif Ali Shaik <saifshine7@gmail.com>
2019-07-23 15:28:46 +0530

    code rewritten and phase 2 completed

--- Freshdesk/v2/simple_events_api_app/app/app.js
+++ Freshdesk/v2/simple_events_api_app/app/app.js
@@ -2,38 +2,43 @@
  * @desc - Whenever an agent clicks on the send reply button in the ticket
  * details page, this app displays a success notification.
  */
-$(document).ready( () => {
-  /** Promise .then()
-   * @param - resolve block, reject block
-   */
-  app.initialized().then((_client) => { // Obtains a Client Object
-    var client = _client;
-    /** [on eventCallback is triggered]
-     * 1. Interface API is used.
-     * 2. title field supports on 30 characters
-     * Result: Notification appears on the right top corner with the 'title'
-     * and 'description'
-      */
-    var eventCallback = () => {
-      client.interface.trigger("showNotify", {
-        type: "success",
-        message: {
-          title: "Success",
-          description: "Your message has been sent"
-        }
-      });
-    };
-    /** Agent Clicking on 'Send' button(event), 'eventCallback' method is invoked.  */
-    client.events.on("ticket.sendReply", eventCallback);
-  },()=>{ 
-    client.interface.trigger("showNotify",{
-      type:"warning",
-      message:{
-        title: "Failed to connect",
-        description:"Error: Your message has not been sent"
-      }
-    });
-    console.log("Failed to get");
-  });
+let showNotification, showError, init, client;
 
+$(document).ready(function () {
+  init();
 });
+
+init = function () {
+  console.log('App init invoked');
+  app.initialized().then(function (_client) {
+    showNotification(_client);
+  }, function (err) {
+    showError(err);
+  });
+}
+
+showNotification = function (clientObj) {
+  console.log("Inside showNotification method");
+  client = clientObj;
+  const notifyReply = function () {
+    client.interface.trigger("showNotify", {
+      type: "success",
+      message: {
+        title: "Success",
+        description: "Your message has been sent"
+      },
+    });
+  }
+  client.events.on("ticket.sendReply", notifyReply);
+}
+
+showError = function (err) {
+  console.log("Inside showError method");
+  client.interface.trigger("showNotify", {
+    type: "warning",
+    message: {
+      title: "Failed to connect",
+      description: "Error: Your message has not been sent"
+    }
+  });
+}
