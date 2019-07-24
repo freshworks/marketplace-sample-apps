78edf2fc5f39786f7a5054b421ab585b681e7171
Saif Ali Shaik <saifshine7@gmail.com>
2019-07-24 15:29:50 +0530

    fdk validate - Handle err

--- Freshdesk/v2/simple_datastorage_app/app/app.js
+++ Freshdesk/v2/simple_datastorage_app/app/app.js
@@ -6,82 +6,82 @@
 
 let noteKey;
 
-$(document).ready(()=>{
-(function(){
-  app.initialized(_client).then(()=>{
-    window.client = _client;
-    client.events.on('app.activated',()=>{
-      fetchInfo(()=>{
-        displayNote();
-        $("#note-save").click(()=>{
-          savenote();
+$(document).ready(() => {
+  (function () {
+    app.initialized(_client).then(() => {
+      window.client = _client;
+      client.events.on('app.activated', () => {
+        fetchInfo(() => {
+          displayNote();
+          $("#note-save").click(() => {
+            savenote();
+          });
+          $("#note-delete").click(() => {
+            deletenote();
+          });
         });
-        $("#note-delete").click(()=>{
-          deletenote();
+      })
+    }, (err) => {
+      showerror(err);
+    });
+
+
+    function showerror(err) {
+      client.interface.trigger("showNotify", {
+        type: "warning", title: "Warning",
+        message: `Error: App is facing issues during initialzation - ${err}`
+      }).then(function (data) {
+        console.log(`Err: Interface API - ${data}`);
+      }).catch(function (error) {
+        console.log(`Some error Encountered: ${error}`);
+      });
+    }
+
+    function savenote() {
+      jQuery("#note-save").click(() => {
+        var noteData = jQuery("#note").val();
+        if (noteData == '') {
+          notify('warning', 'Note is empty');
+          return;
+        }
+        client.db.set(noteKey, { note: noteData }).then(() => {
+          notify('success', 'Note has been stored');
+        }, () => {
+          notify('danger', 'Error storing the note');
         });
       });
-    })
-  },(err)=>{
-    showerror();
-  });
+    }
 
+    function deletenote() {
+      client.db.delete(noteKey)
+        .then(function () {
+          jQuery("#note").val('');
+          notify('success', 'Note has been deleted');
+        }, function () {
+          notify('danger', 'Error deleting the note');
+        });
+    }
 
-  function showerror(){
-    client.interface.trigger("showNotify", {
-      type: "warning", title: "Warning",
-      message: "Error: App is facing issues during initialzation"
-    }).then(function(data) {
-    console.log(`Err: Interface API - ${data}`);
-    }).catch(function(error) {
-    console.log(`Some error Encountered: ${error}`);
-    });
-  }
-
-  function savenote(){
-    jQuery("#note-save").click(() => {
-      var noteData = jQuery("#note").val();
-      if (noteData == '') {
-        notify('warning', 'Note is empty');
-        return;
-      }
-      client.db.set(noteKey, { note: noteData}).then(() => {
-        notify('success', 'Note has been stored');
-      }, () => {
-        notify('danger', 'Error storing the note');
+    function displayNote() {
+      client.db.get(noteKey).then((data) => {
+        $("#note").val(data.note);
       });
-    });
-  }
+    }
 
-  function deletenote(){
-    client.db.delete(noteKey)
-      .then(function() {
-        jQuery("#note").val('');
-        notify('success', 'Note has been deleted');
-      }, function() {
-        notify('danger', 'Error deleting the note');
+    function notify(status, message) {
+      client.interface.trigger('showNotify', {
+        type: status,
+        message: message
       });
-  }
+    }
 
-  function displayNote(){
-    client.db.get(noteKey).then((data)=>{
-      $("#note").val(data.note);
-    });
-  }
-
-  function notify(status,message){
-    client.interface.trigger('showNotify',{
-      type: status,
-      message: message
-    });
-  }
-
-  function fetchInfo(callback){
-    client.data.get('loggedInUser').then((userData)=>{
-      client.data.get('ticket').then((ticketData)=>{
-        noteKey = `${userData.loggedInUser.id} : ${ticketData.ticket.id}`;
-        callback();
+    function fetchInfo(callback) {
+      client.data.get('loggedInUser').then((userData) => {
+        client.data.get('ticket').then((ticketData) => {
+          noteKey = `${userData.loggedInUser.id} : ${ticketData.ticket.id}`;
+          callback();
+        });
       });
-    });
-  }
-})();
+    }
+  })();
 });
