62fee67ad35c9d51d11123959f9c259ffac74e39
Hem <hemchander.23@gmail.com>
2019-09-16 12:43:54 +0200

    FreshHuddle Live! - Complete fundamentals L1 app

--- Training/fundamentals/level1/app/app.js
+++ Training/fundamentals/level1/app/app.js
@@ -29,7 +29,36 @@ function onLoadClickEventHandler() {
 }
 
 // 1.2 Paste the code for createFreshdeskTicket() here 👇🏼 !
-
+/**
+* 1.2 Function to create a Freshdesk ticket ! 🤘
+* @param {String} title          Ticket title
+* @param {String} description    Ticket description
+* @param {String} email          email of the user that creates ticket
+*/
+function createFreshdeskTicket(title, description, email) {
+  client.request.post("https://<%=iparam.freshdesk_subdomain%>.freshdesk.com/api/v2/tickets", {
+    headers: {
+      Authorization: "Basic <%= encode(iparam.freshdesk_api_key)%>",
+      "Content-Type": "application/json;charset=utf-8"
+    },
+    body: JSON.stringify({
+      description: `${description}`,
+      email: `${email}`,
+      priority: 1,
+      status: 2,
+      subject: `${title}`
+    })
+  })
+    .then(function () {
+      showNotification('success', 'Ticket is successfully created');
+      //Clears user input after posting data
+      clearInputfields();
+    })
+    .catch(function (error) {
+      console.error(error);
+      showNotification('danger', 'Unable to create ticket');
+    });
+}
 
 /**
  * Function to show notifications to the user
