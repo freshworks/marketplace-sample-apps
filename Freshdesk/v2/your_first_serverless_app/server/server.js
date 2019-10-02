9d57735df36443695630a5506b6cbc602e1b27d8
Saif Ali Shaik <saifshine7@gmail.com>
2019-10-02 18:25:05 +0530

    make es5 compatible

--- Freshdesk/v2/your_first_serverless_app/server/server.js
+++ Freshdesk/v2/your_first_serverless_app/server/server.js
@@ -1,26 +1,33 @@
+"use strict";
+
 /**
  * @description -
  * Every time a new ticket is created, this app prints a "Hello {requester name}"
  * message to the terminal window. (Where you run 'fdk run')
  * @info - https://developers.freshdesk.com/v2/docs/your-first-serverless-app/
  */
-
 exports = {
   events: [
-    { event: 'onTicketCreate', callback: 'onTicketCreateHandler' }
+    {
+      event: "onTicketCreate",
+      callback: "onTicketCreateHandler"
+    }
   ],
+
   /**
    * When ticket is created, some details from the payload sent is logged.
    * @param {string} - A JSON string
    */
-  onTicketCreateHandler: function (payload) {
-    const details = `${payload['data']['requester']['name']}`;
-    const email = `${payload['data']['requester']['email']}`;
-    const mobile = `${payload['data']['requester']['mobile']}`;
-    console.log(JSON.stringify({
-      Details: details,
-      email: email,
-      mobile: mobile
-    }));
+  onTicketCreateHandler: function onTicketCreateHandler(payload) {
+    var details = "".concat(payload["data"]["requester"]["name"]);
+    var email = "".concat(payload["data"]["requester"]["email"]);
+    var mobile = "".concat(payload["data"]["requester"]["mobile"]);
+    console.log(
+      JSON.stringify({
+        Details: details,
+        email: email,
+        mobile: mobile
+      })
+    );
   }
 };
