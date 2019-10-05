a6c6fd7a834a84e76cb61c0da513885bee08b603
Gil <gil707@gmail.com>
2019-10-05 14:30:18 +0300

    Convert ES6 code to ES5

--- Freshdesk/v2/instance_api_app/app/modal.js
+++ Freshdesk/v2/instance_api_app/app/modal.js
@@ -1,22 +1,23 @@
 /**
- * This script file acts for modal.html 
- * 
+ * This script file acts for modal.html
+ *
  * This demonstrates, Using instance API to retrieve the data received from
  * the parent location.
  */
 
-$(document).ready(() => {
-  app.initialized().then((_client) => {
-    const client = _client;
-    client.instance.context().then((context) => {
-      console.log("Modal instance API context", context);
-      /* Output: Modal instance API context 
-      * { instanceId: "4",   location: "modal", parentId: "1",
-      * modalData: {name: "James", email: "James@freshdesk.com"} 
-      * }" 
-      * */
-      $('#name').val(context.data.name);
-      $('#email').val(context.data.email);
+$(document).ready(function () {
+    app.initialized().then(function (_client) {
+        var client = _client;
+        client.instance.context().then(function (context) {
+            console.log("Modal instance API context", context);
+            /* Output: Modal instance API context
+            * { instanceId: "4",   location: "modal", parentId: "1",
+            * modalData: {name: "James", email: "James@freshdesk.com"}
+            * }"
+            * */
+
+            $('#name').val(context.data.name);
+            $('#email').val(context.data.email);
+        });
     });
-  });
 });
