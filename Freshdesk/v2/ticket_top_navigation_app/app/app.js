4fbf8546de34b3ddba3f142d87f7aab91b938d41
Gil <gil707@gmail.com>
2019-10-05 14:34:32 +0300

    Convert ES6 code to ES5 for top_navigation_app

--- Freshdesk/v2/ticket_top_navigation_app/app/app.js
+++ Freshdesk/v2/ticket_top_navigation_app/app/app.js
@@ -3,36 +3,38 @@
  * shown.
  */
 
-$(document).ready( () => {
-  // Initialize channel
-  app.initialized().then((_client) => {
-    let client = _client;
-    // App activate callback
-    client.events.on('app.activated', () => {
-      // Open modal to collect params to start timer Interface API
-      client.interface.trigger('showModal', { 
-        title: 'Start timer', 
-        template: 'startTimer.html' })
-      .then(null, (error) => {
-        client.interface.trigger('showNotify', { 
-          type: 'error', 
-          message: 'Some error has occured in \'Start timer app\'.'});
-      });
-    });
+$(document).ready(function () {
+    // Initialize channel
+    app.initialized().then(function (_client) {
+        var client = _client; // App activate callback
 
-    client.instance.receive((event) => {
-      let data = event.helper.getData();
-      if (data.message.agent) {
-        // start the timer
-        client.interface.trigger('start', { 
-          id: 'timer', 
-          value: data.message 
+        client.events.on('app.activated', function () {
+            // Open modal to collect params to start timer Interface API
+            client.interface.trigger('showModal', {
+                title: 'Start timer',
+                template: 'startTimer.html'
+            }).then(null, function (error) {
+                client.interface.trigger('showNotify', {
+                    type: 'error',
+                    message: 'Some error has occured in \'Start timer app\'.'
+                });
+            });
         });
-      }
+        client.instance.receive(function (event) {
+            var data = event.helper.getData();
+
+            if (data.message.agent) {
+                // start the timer
+                client.interface.trigger('start', {
+                    id: 'timer',
+                    value: data.message
+                });
+            }
+        });
+    }, function (error) {
+        client.interface.trigger('showNotify', {
+            type: 'error',
+            message: 'Some error has occured in \'Start timer app\'.'
+        });
     });
-  }, (error) => {
-    client.interface.trigger('showNotify', { 
-      type: 'error', 
-      message: 'Some error has occured in \'Start timer app\'.'});
-  });
 });
