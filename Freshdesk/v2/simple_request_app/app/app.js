1be2ec0c98248f10949acb9a89c08d201b126c4c
Saif Ali Shaik <saifshine7@gmail.com>
2019-10-11 23:28:23 +0530

    remove back tick sting

--- Freshdesk/v2/simple_request_app/app/app.js
+++ Freshdesk/v2/simple_request_app/app/app.js
@@ -1,55 +1,56 @@
+"use strict";
+
 /**
  * @description - This app makes REST API calls to httpbin.org.
  *
  * 1. Request API - GET, POST, PUT & DELETE
  */
-
-const BASE_URL = "https://httpbin.org/";
-
-
- $(document).ready(function(){
-  (function(){
-    app.initialized().then((_client)=>{
+var BASE_URL = "https://httpbin.org/";
+$(document).ready(function () {
+  (function () {
+    app.initialized().then(function (_client) {
       window.client = _client;
-      client.events.on('app.activated', init);
+      client.events.on("app.activated", init);
     });
 
-    function init(){
-      $(".request-btn").click((e)=>{
-        let method = e.target.getAttribute('method');
-        const url = BASE_URL + method;
-        const options = {
-        "headers" : {
-          "Content-Type": "application/json"
-        }
+    function init() {
+      $(".request-btn").click(function (e) {
+        var method = e.target.getAttribute("method");
+        var url = BASE_URL + method;
+        var options = {
+          headers: {
+            "Content-Type": "application/json"
+          }
         };
-        req(method,url,options);
+        req(method, url, options);
       });
     }
-
     /**
      * The method will make a request to httpbin.org. There parameters define the kind of request.
      *
      * @method {String} - GET/POST request
      * @url {String} - The URL of the endpoint
-     * 
+     *
      */
 
-    function req(method,url,options){
-      client.request[method](url, options)
-      .then(() => {
-        displayStatus('success',`${method.toUpperCase()} request successful`);
-      }, () => {
-        displayStatus('danger', `${method.toUpperCase()} request failed`);
-      });
+    function req(method, url, options) {
+      client.request[method](url, options).then(
+        function () {
+          displayStatus("success", "".concat(method.toUpperCase(), " request successful")
+          );
+        },
+        function () {
+          displayStatus("danger", "".concat(method.toUpperCase(), " request failed")
+          );
+        }
+      );
     }
 
-    function displayStatus(type, message){
-      client.interface.trigger('showNotify',{
+    function displayStatus(type, message) {
+      client.interface.trigger("showNotify", {
         type: type,
         message: message
       });
     }
-
   })();
- });
+});
