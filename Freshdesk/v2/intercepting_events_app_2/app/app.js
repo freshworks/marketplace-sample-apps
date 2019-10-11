c25f958518673bee3b637cbc436c0aae8291671a
Saif Ali Shaik <saifshine7@gmail.com>
2019-10-11 15:00:25 +0530

    Intercepting events app 2: ES5 revert

--- Freshdesk/v2/intercepting_events_app_2/app/app.js
+++ Freshdesk/v2/intercepting_events_app_2/app/app.js
@@ -1,33 +1,35 @@
-/**
- * @description - Whenever a ticket is expected to be closed, this app checks
- * if there are any tags attached to the ticket, Upon no tags, it halts the close
- * event and displays an error notification.
- *
- *  - Using data API to fetch the ticket's data.
- *  - Using intercept APIs to check and stop the close action if there are no
- * tags attached to the ticket.
- *
- */
+"use strict";
 
-$(document).ready(() => {
-  app.initialized().then((_client) => {
-    let client = _client;
-    let eventCallback = function (event) {
+$(document).ready(function () {
+  app.initialized().then(function (_client) {
+    var client = _client;
+
+    var eventCallback = function eventCallback(event) {
       /** @fires - Data API */
-      client.data.get("ticket").then((data) => {
-        /** If there is atleast 1 tag, allow the original event to continuve */
-        if (data.ticket.tags.length > 0) {
-          event.helper.done();
+      client.data.get("ticket").then(
+        function (data) {
+          /** If there is atleast 1 tag, allow the original event to continue */
+          if (data.ticket.tags.length > 0) {
+            event.helper.done();
+          } else {
+            event.helper.fail("Please make sure that at least one tag is attached to this ticket");
+          }
+        },
+        function (error) {
+          client.interface.trigger("showNotify", {
+            type: "error",
+            message: {
+              title: "Error",
+              description: "Error while trying to fetch data"
+            }
+          });
+          console.error(error);
         }
-        else {
-          event.helper.fail("Please make sure that at least one tag is attached to this ticket");
-        }
-      },
-        (error) => {
-          client.interface.trigger("showNotify", { type: "error", message: { title: "Error", description: "Error while trying to fetch data" } });
-        });
+      );
     };
 
-    client.events.on("ticket.closeTicketClick", eventCallback, { intercept: true });
+    client.events.on("ticket.closeTicketClick", eventCallback, {
+      intercept: true
+    });
   });
 });
