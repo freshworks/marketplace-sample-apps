1b99c34722ba847b228137c1accc7a13bb5bece3
Raviraj Subramanian <raviraj.subramanian@freshworks.com>
2020-04-30 17:17:39 +0530

    Added assumptions to Readme and unified logs format

--- Freshworks-Samples/Freshchat/conversations-data-storage-helper/server/server.js
+++ Freshworks-Samples/Freshchat/conversations-data-storage-helper/server/server.js
@@ -22,12 +22,12 @@ exports = {
     const encodedConversationId = helper.encode(payload.conversation_id);
     retrieveNote(encodedConversationId).then(
       function (data) {
-        console.info('Seccessfully fetched the note data from db');
+        console.info('Successfully fetched the note data from db');
         console.info(data);
         renderData(null, data);
       },
       function (error) {
-        console.error('failed to store the key-value pair successfully');
+        console.error('Failed to store the key-value pair');
         console.error(error);
         renderData(error);
       });
