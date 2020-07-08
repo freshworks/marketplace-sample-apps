4b712b82c0310c2eeb3e45108b57e077d8d50525
Hem <hemchander.23@gmail.com>
2020-07-08 17:54:48 +0530

    ⚒️ Remove getConfigs() hook getConfigs() can be used to persist field states  in dynamic iparams.

--- Freshworks-Samples/App-Development-Features/Configuration-Features/dynamic_iparams/README.md
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/dynamic_iparams/README.md
@@ -1,11 +1,11 @@
 # Dynamic Installation Page ✨
 
 ## Description
-This sample app demonstrates how iparam callbacks can be used to improve validation and make the installation parameters much more dynamic. This approach cuts the need for custom installation parameters and leverages the simplicity and power of standard iparams (iparams.json).
+This sample app demonstrates how iparam callbacks can be used to enable custom validation and make the installation parameters much more dynamic. This approach cuts the need for custom installation parameters and leverages the simplicity and power of standard iparams (iparams.json).
 
 ## Screenshots
 
-![Preview of iparam callback validation app](./app/assets/img/preview.png)
+![Preview of iparam callback validation app](./app/assets/img/preview.jpg)
 
 ## Features Demonstrated
 
@@ -25,7 +25,8 @@ App location: ticket details page
 ## Procedure to run the app
 
 1. Run the app locally using the [`fdk run`](https://developers.freshdesk.com/v2/docs/freshworks-cli/#run) command.
-2. Go to `http://localhost:10001/custom_configs` in your browser to test the installation page
-3. You can add new callback functions associated with iparam fields in `iparams.js`
+2. Go to `http://localhost:10001/custom_configs` in your browser to test the installation page.
+3. You can add new callback functions associated with iparam fields in `iparams.json` by specifying the callback name & in `iparams.js`by defining the corresponding callback function.
 4. You can also modify the code directly in the `iparams.js` file to see the changes in the Installation pages.
+
 
--- Freshworks-Samples/App-Development-Features/Configuration-Features/dynamic_iparams/config/assets/iparams.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/dynamic_iparams/config/assets/iparams.js
@@ -2,7 +2,8 @@
 
 /**
  * App lifecycle method to initialize the app and to obtain the `client` object
- * More details on Dynamic Installation parameters can be found at https://developers.freshdesk.com/v2/docs/installation-parameters/#dynamic_install_page
+ * More details on Dynamic Installation parameters can be found at the link below ⬇️
+ * https://developers.freshdesk.com/v2/docs/installation-parameters/#dynamic_install_page
  */
 app.initialized().then(
   function (_client) {
@@ -17,8 +18,8 @@ app.initialized().then(
 
 /**
  * Using this iparam callback function, we are validating the details using a third-party API. 
- * In this case, we are making use of `httpbin.org` to return 200 OK status. 
- * In real-world, this could be a valid third-party API that can return an appropriate status code
+ * In this case,for example, we are making use of `httpbin.org` to return 200 OK status. 
+ * In real-world, this could be a valid third-party API that can return an appropriate status code indicating the status of validation
  * Payload and other options can be specified using `options`
  * 
  * @param {string} newValue The new value of the iparam field
@@ -49,64 +50,11 @@ function checkAccountID(newValue) {
   return Promise.resolve();
 }
 /**
- * When the contact method changes, hide or show appropriate fields
+ * When the contact method changes, just display the options
+ * To set the list of possible options to choose from, use -> utils.set("<iparam_field>", { values: ['Opt1', 'Opt2', 'Opt3'] });
  */
 function contactMethodChanged() {
   //Let us get the selected options for contact methods
   const cm = utils.get("contact_methods");
-  toggleFieldsVisibility(cm);
-}
-/**
- * getConfigs() can also be used to perform certain operations on fields while the iparams page is loaded
- * @param {object} data 
- */
-function getConfigs(data) {
-  const cm = data.contact_methods;
-  toggleFieldsVisibility(cm);
-  setFieldValues(["twitter_id", "twitter_tags", "contact_methods", "domain_name", "api_key", "mobile", "tags"], data);
-  populateTags();
-}
-/**
- * Populate the multi-choice field `tags` based on the value of `twitter_tags` field
- */
-function populateTags() {
-  const tagCSV = utils.get('twitter_tags');
-  utils.set('tags', {
-    'values': tagCSV.split(",")
-  });
-}
-/**
- *  Dynamically enable / disable standard iparam fields using the utility methods
- * @param {object} cm 
- */
-function toggleFieldsVisibility(cm) {
-  if (!Array.isArray(cm))
-    return console.error("Something went wrong while toggling field visibility");
-  utils.set("twitter_id", {
-    visible: cm.includes("Twitter ID")
-  });
-  utils.set("twitter_tags", {
-    visible: cm.includes("Twitter ID")
-  });
-  utils.set("tags", {
-    visible: cm.includes("Twitter ID")
-  });
-  utils.set("mobile", {
-    visible: cm.includes("Mobile")
-  });
-}
-/**
- * Set the iparam field values based on the iparam field keys that are passed.
- * Utility methods can be used to set a field value
- * @param {object} fields 
- * @param {object} data
- */
-function setFieldValues(fields, data) {
-  if (!Array.isArray(fields))
-    return console.error("Something went wrong while setting field values. Ensure that an array of field names matching the iparam key names in iparams.json is passed");
-  fields.map(function (field) {
-    utils.set(field, {
-      'value': data[field]
-    });
-  });
+  console.log(cm);
 }
