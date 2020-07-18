4e102468aaa4dbc102e7aa98adba32a6de6218cc
Saif Ali Shaik <saifshine7@gmail.com>
2020-07-18 08:21:15 +0530

    render product specific page

--- Freshworks-Samples/Omni-App-Samples/Custom-Installation-Page_Omni/config/assets/iparams.js
+++ Freshworks-Samples/Omni-App-Samples/Custom-Installation-Page_Omni/config/assets/iparams.js
@@ -1,21 +1,29 @@
 let client;
 
+const titleHTML = document.querySelector("header.product-specific.title");
+const subTitleHTML = document.querySelector("legend.product-specific.subtitle");
+
 function onDocumentReady() {
   app
     .initialized()
     .then(function (_client) {
       client = _client;
-      productSpecificPage();
+      let productName =
+        String(client.context.product) == "freshsales"
+          ? "Freshsales"
+          : "Freshworks CRM";
+      productSpecificPage(productName);
     })
     .catch((err) => {
       logError(err);
     });
 }
 
-function productSpecificPage() {
-  let productName = client.context.product;
-  console.log(productName);
-  document.querySelector('')
+function productSpecificPage(productName) {
+  titleHTML.innerHTML = `Omni - Custom Installation Page: <span>
+  ${productName}</span > 🌍`;
+  subTitleHTML.innerHTML = `${productName} Specific Elements
+        Implementation`;
 }
 
 function logError(err) {
