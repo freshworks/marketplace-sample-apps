a4234206ec988cb574cec7e7a6e5395aea6127f2
Saif Ali Shaik <saifshine7@gmail.com>
2020-07-18 12:01:04 +0530

    local testing complete

--- Freshworks-Samples/Omni-App-Samples/Custom-Installation-Page_Omni/app/app.js
+++ Freshworks-Samples/Omni-App-Samples/Custom-Installation-Page_Omni/app/app.js
@@ -1,9 +1,26 @@
-var client;
-$(document).ready(function () {
-  app.initialized().then(function (_client) {
-    client = _client;
-    client.events.on("app.activated", function () {
-      //Your code here
-    });
-  });
-});
+let client;
+
+function init() {
+  app.initialized().then(
+    function (_client) {
+      client = _client;
+      client.events.on("app.activated", logInstallationParameters());
+    },
+    (e) => errorLogger(e)
+  );
+}
+
+function errorLogger(e) {
+  console.error("The follwoing Error occured -", error);
+}
+
+function logInstallationParameters() {
+  client.iparams.get().then(
+    (iparamsInfo) => {
+      console.info(iparamsInfo);
+    },
+    (error) => errorLogger(error)
+  );
+}
+
+$(document).ready(init());
