c54d4b64b2c91337c50510b0ac855f7fe48c8a60
Saif Ali Shaik <saifshine7@gmail.com>
2020-08-29 16:22:07 +0530

    make iparams.js semantic

--- Freshworks-Samples/Omni-App-Samples/iparams-json-omni-app/config/assets/iparams.js
+++ Freshworks-Samples/Omni-App-Samples/iparams-json-omni-app/config/assets/iparams.js
@@ -1,44 +1,52 @@
-function onFormLoad() {
-  console.log(
-    'Onformload callback function is called when iparam page is opened',
-  );
-  app.initialized().then(
-    function (client) {
-      // let product = client.context.product;
-      let product = client.context.productContext.name;
+var runtimeProductName;
+var fsalesInstallationPage = function (utils) {
+  //Changing Hint Text Based on Product
+  utils.set('ApiKey', {hint: 'Enter APIKey of your Freshsales Account'});
+  utils.set('TextDomain', {
+    hint: 'Enter Full Domain of your Freshsales Account',
+  });
+  //Changing Display Name Based on Product
+  utils.set('ApiKey', {label: 'APIKey_Freshsales'});
+  utils.set('TextDomain', {label: 'FullDomain_Freshsales'});
+  //To Hidden the field specific to FCMR
+  utils.set('FieldToDisplayInFCRM', {visible: false});
+};
+var fcrmInstallationPage = function (utils) {
+  utils.set('ApiKey', {hint: 'Enter APIKey of your FCRM Account'});
+  utils.set('TextDomain', {
+    hint: 'Enter Full Domain of your FCRM Account',
+  });
+  //Changing Display Name Based on Product
+  utils.set('ApiKey', {label: 'APIKey_FCRM'});
+  //To Hidden the field specific to Fslaes
+  utils.set('FieldToDisplayInFSales', {visible: false});
+};
 
-      if (product == 'freshsales') {
-        //Changing Hint Text Based on Product
-        utils.set('ApiKey', {hint: 'Enter APIKey of your Freshsales Account'});
-        utils.set('TextDomain', {
-          hint: 'Enter Full Domain of your Freshsales Account',
-        });
-        //Changing Display Name Based on Product
-        utils.set('ApiKey', {label: 'APIKey_Freshsales'});
-        utils.set('TextDomain', {label: 'FullDomain_Freshsales'});
-        //To Hidden the field specific to FCMR
-        utils.set('FieldToDisplayInFCRM', {visible: false});
-      } else if (product == 'freshworks_crm') {
-        //Changing Hint Text Based on Product
-        utils.set('ApiKey', {hint: 'Enter APIKey of your FCRM Account'});
-        utils.set('TextDomain', {
-          hint: 'Enter Full Domain of your FCRM Account',
-        });
-        //Changing Display Name Based on Product
-        utils.set('ApiKey', {label: 'APIKey_FCRM'});
-        //To Hidden the field specific to Fslaes
-        utils.set('FieldToDisplayInFSales', {visible: false});
+function onFormLoad() {
+  console.log('OnFormLoad() function fired 🧨');
+  app.initialized().then(
+    function getClientObj(client) {
+      runtimeProductName = client.context.productContext.name;
+
+      /**
+       * App trying to learn which product it's runtime is going to be on
+       * render relavant installation page.
+       */
+
+      if (runtimeProductName == 'freshsales') {
+        fsalesInstallationPage(utils);
+      } else if (runtimeProductName == 'freshworks_crm') {
+        fcrmInstallationPage(utils);
       } else {
         console.log('ERROR:Missing Expected product from client');
       }
     },
-    function (error) {
-      //If unsuccessful
-      console.log('ERROR:Problem in fetching product from client');
+    function onFailProductIdentification(error) {
+      console.error('ERROR:Problem in fetching product from client');
     },
   );
 }
 
 function onFormUnload() {
-  console.log('OnformUnload callback called');
+  console.log('OnFormUnload function fired 🚒');
 }
