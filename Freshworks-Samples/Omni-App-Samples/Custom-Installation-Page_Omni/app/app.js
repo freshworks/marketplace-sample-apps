d28bdea076f74a265098fdd0d82f11d91a720eb3
Saif Ali Shaik <saifshine7@gmail.com>
2020-09-01 11:14:22 +0530

    refactor code

--- Freshworks-Samples/Omni-App-Samples/Custom-Installation-Page_Omni/app/app.js
+++ Freshworks-Samples/Omni-App-Samples/Custom-Installation-Page_Omni/app/app.js
@@ -1,14 +1,15 @@
 var client;
 
 var errorLogger = function (error) {
-  console.error("The following Error occured -", error);
+  console.error('The following Error occured.");
+  console.error(error);
 };
 
 function init() {
   app.initialized().then(function getClient(_client) {
     client = _client;
-    client.events.on("app.activated", function onActivate() {
-      console.info("App is Activated");
+    client.events.on('app.activated', function onActivate() {
+      console.info('App is Activated');
     });
   }, errorLogger);
 }
