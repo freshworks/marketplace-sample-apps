c2ec519a21716737da0b3a8b705a1393840290e8
Shaik Saif Ali <saifali.shaik@freshworks.com>
2020-09-02 16:02:23 +0530

    Custom Installation Page Demonstration for Omni Apps (#166)
    * create Omni App Samples folder
    
    * half of readme is captured
    
    * complete markdown overview
    
    * rename to readme.md
    
    * Omni app
    
    * custom installation page's README.md file
    
    * manifest.json is updated
    
    * create assets for iparams.html
    
    * custom installation page ui html elements added
    
    * declare logError() and productSpecificPage()
    
    * render product specific page
    
    * local testing complete
    
    * app readme update
    
    * add button to log installation parameters
    
    * fix typo: on
    
    Co-authored-by: Raviraj Subramanian <raviraj.subramanian@freshworks.com>
    
    * fix typo: is
    
    Co-authored-by: Raviraj Subramanian <raviraj.subramanian@freshworks.com>
    
    * fix type: the
    
    Co-authored-by: Raviraj Subramanian <raviraj.subramanian@freshworks.com>
    
    * browser log should be console.error
    
    Co-authored-by: Raviraj Subramanian <raviraj.subramanian@freshworks.com>
    
    * update platform-version
    
    * multiple changes
    
    * convert to named function when logging installation parameters
    
    * convert anonymous function to named: onActivate()
    
    * changes
    
    * change error callback to arrow function: errorLogger
    
    * update iparams.js be reducing error to handled
    
    * way to access value of productContext updates
    
    * passed errorLogger by reference
    
    * convert open variables to object customPageFields
    
    * move iparams.js to the bottom of the page
    
    * refactor code
    
    * change function expression to function declaraion
    
    * remove unnecessary comments
    
    * remove domain as it is not needed for omni apps
    
    * updates: README.md
    
    * minor improvements
    
    Co-authored-by: Raviraj Subramanian <raviraj.subramanian@freshworks.com>

---
+++ Freshworks-Samples/Omni-App-Samples/Custom-Installation-Page_Omni/app/app.js
@@ -0,0 +1,24 @@
+var client;
+
+function errorLogger(error) {
+  console.error('The following Error occured');
+  console.error(error);
+}
+
+function init() {
+  app.initialized().then(function getClient(_client) {
+    client = _client;
+    client.events.on('app.activated', function onActivate() {
+      console.info('App is Activated');
+    });
+  }, errorLogger);
+}
+
+function logInstallationParameters() {
+  init();
+  client.iparams.get().then(function getiparams(info) {
+    console.info(info);
+  }, errorLogger);
+}
+
+$(document).ready(init);
