9d6bb8fa6aea8ba7f0937d23a48be52eb8727542
Shaik Saif Ali <saifali.shaik@freshworks.com>
2020-09-02 17:15:15 +0530

    Iparams json omni (#178)
    * generate app using fdk create templates
    
    * remove unsupported app placeholders
    
    * remove domain fields. developers are expected to grab from productContext
    
    * make iparams.js semantic
    
    * Hide fields those are irrelevant to other
    
    * remove test data json
    
    * remove unnecessary comments
    
    * Readme update
    
    * pass function reference

---
+++ Freshworks-Samples/Omni-App-Samples/iparams-json-omni-app/config/assets/iparams.js
@@ -0,0 +1,41 @@
+var runtimeProductName;
+var fsalesInstallationPage = function (utils) {
+  utils.set('ApiKey', {hint: 'Enter APIKey of your Freshsales Account'});
+  t;
+  utils.set('ApiKey', {label: 'APIKey_Freshsales'});
+  utils.set('FieldToDisplayInFCRM', {visible: false});
+};
+var fcrmInstallationPage = function (utils) {
+  utils.set('ApiKey', {hint: 'Enter APIKey of your FCRM Account'});
+  utils.set('ApiKey', {label: 'APIKey_FCRM'});
+  utils.set('FieldToDisplayInFSales', {visible: false});
+};
+
+function onFormLoad() {
+  console.log('OnFormLoad() function fired 🧨');
+  app.initialized().then(
+    function getClientObj(client) {
+      runtimeProductName = client.context.productContext.name;
+
+      /**
+       * App trying to learn which product it's runtime is going to be on
+       * render relavant installation page.
+       */
+
+      if (runtimeProductName == 'freshsales') {
+        fsalesInstallationPage(utils);
+      } else if (runtimeProductName == 'freshworks_crm') {
+        fcrmInstallationPage(utils);
+      } else {
+        console.log('❗️:Missing Expected product from client');
+      }
+    },
+    function onFailProductIdentification(error) {
+      console.error('ERROR:Problem in fetching product from client');
+    },
+  );
+}
+
+function onFormUnload() {
+  console.log('OnFormUnload function fired 🚒');
+}
