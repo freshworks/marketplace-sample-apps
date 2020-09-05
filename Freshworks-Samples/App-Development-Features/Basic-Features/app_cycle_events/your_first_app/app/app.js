7ad9fa1b67d363dd995d0cfdc373922a07b7e3e6
Saif Ali Shaik <saifshine7@gmail.com>
2020-09-05 22:35:53 +0530

    refactor your first app code

--- Freshworks-Samples/App-Development-Features/Basic-Features/app_cycle_events/your_first_app/app/app.js
+++ Freshworks-Samples/App-Development-Features/Basic-Features/app_cycle_events/your_first_app/app/app.js
@@ -1,24 +1,37 @@
-/**
- * @desc - Sample App shows fectching Contact from Data API and shows on Ticket
- * sidebar
- */
+var client;
 
-$(document).ready(function() {
-  app.initialized().then(function(_client) {
-    const client = _client;
-    client.events.on('app.activated', function () {
-      getContactData(client);
-    });
-  });
+isDocumentReady();
 
-  function getContactData(client) {
-    client.data.get('contact').then(function (data) {
-      $('#apptext').text(`Ticket created by ${data.contact.name}`);
-      congratulations();
-    });
-  }
+function startAppRender() {
+  app
+    .initialized()
+    .then(function loadContactData(_client) {
+      client = _client;
+      client.events.on('app.activated', renderContactName);
+    })
+    .catch(errorHandler);
+}
 
-  function congratulations() {
-    $('.content').text(`Congratulations on creating your first app`);
-  }
-});
+function renderContactName() {
+  var textElement = document.getElementById('apptext');
+  client.data
+    .get('contact')
+    .then(function getData(data) {
+      textElement.innerHTML = `Ticket created by ${data.contact.name}`;
+    })
+    .catch(errorHandler);
+}
+
+function appLoading() {
+  console.info('There is an error in rendering the app!');
+}
+
+function errorHandler(err) {
+  console.error(`App failed to initialize because...`);
+  console.error(err);
+}
+
+function isDocumentReady() {
+  if (document.readyState != 'loading') appLoading();
+  else document.addEventListener('DOMContentLoaded', startAppRender);
+}
