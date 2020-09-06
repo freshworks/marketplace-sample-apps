5f15faa76ffcbaa0d33c7359c7c6be08508bb759
Saif Ali Shaik <saifshine7@gmail.com>
2020-09-06 09:29:38 +0530

    req api app

--- Freshworks-Samples/App-Development-Features/Advanced-Features/request_method/request_api/app/app.js
+++ Freshworks-Samples/App-Development-Features/Advanced-Features/request_method/request_api/app/app.js
@@ -1,15 +1,22 @@
-$(document).ready(function () {
-  app.initialized().then(function (client) {
-    client.events.on("app.activated", function () {
-      // sample code for RequestAPI
-      client.request.get("https://httpbin.org/get", { client: true }).then(
-        function (data) {
-          console.log("RequestAPI:", data);
-        },
-        function (error) {
-          console.log("RequestAPI:", error);
-        }
-      );
-    });
-  });
-});
+isDocumentReady();
+
+function startApprender() {
+  app.initialized().then(function getClientObj(client) {
+    let options = { client: true }
+    const displayElement = document.getElementById('apptext');
+    client.request.get("https://httpbin.org/get", options).then((payload) => {
+      displayElement.innerHTML = `httpbin responded with <code>${payload.response}</code>`
+    }, (error) => {
+      console.error('An error occurred during the request..')
+      console.error(error);
+    })
+  })
+}
+
+function isDocumentReady() {
+  if (document.readyState != 'loading') {
+    console.info('Browser waiting until DOM loads...')
+  } else {
+    document.addEventListener('DOMContentLoaded', startApprender);
+  }
+}
