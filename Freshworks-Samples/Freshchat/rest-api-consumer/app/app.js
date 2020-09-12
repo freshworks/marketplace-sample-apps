e78be10a0c0571d9c4316c8083d44b09dfd8cbac
Saif Ali Shaik <saifshine7@gmail.com>
2020-09-12 21:41:19 +0530

    app created

---
+++ Freshworks-Samples/Freshchat/rest-api-consumer/app/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    app.initialized()
+        .then(function (_client) {
+            var client = _client;
+            client.events.on('app.activated',
+                function () {
+                    client.data.get('loggedInAgent')
+                        .then(function (data) {
+                            $('#apptext').text("Agent logged in is " + data.loggedInAgent.email);
+                        })
+                        .catch(function (e) {
+                            client.interface.trigger("showNotify", {
+                                type: "danger",
+                                message: "Unable to retrieve logged-in agent details"
+                            });
+                            console.log('Exception - ', e);
+                        });
+                });
+        });
+});
