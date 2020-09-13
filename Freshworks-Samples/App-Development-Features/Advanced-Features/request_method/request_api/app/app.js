2ecdf8bed7007b4c57e771c330f7b747675fa378
Saif Ali Shaik <saifshine7@gmail.com>
2020-09-13 16:14:20 +0530

    address EOF and naming function name

--- Freshworks-Samples/App-Development-Features/Advanced-Features/request_method/request_api/app/app.js
+++ Freshworks-Samples/App-Development-Features/Advanced-Features/request_method/request_api/app/app.js
@@ -1,22 +1,23 @@
 isDocumentReady();
 
-function startApprender() {
-  app.initialized().then(function getClientObj(client) {
-    let options = { client: true }
-    const displayElement = document.getElementById('apptext');
-    client.request.get("https://httpbin.org/get", options).then((payload) => {
-      displayElement.innerHTML = `httpbin responded with <code>${payload.response}</code>`
-    }, (error) => {
-      console.error('An error occurred during the request..')
-      console.error(error);
+function startAppRender() {
+  app.initialized()
+    .then(function (client) {
+      let options = { client: true }
+      const displayElement = document.getElementById('apptext');
+      client.request.get("https://httpbin.org/get", options).then((payload) => {
+        displayElement.innerHTML = `httpbin responded with <code>${payload.response}</code>`
+      }, (error) => {
+        console.error('An error occurred during the request..')
+        console.error(error);
+      })
     })
-  })
 }
 
 function isDocumentReady() {
   if (document.readyState != 'loading') {
     console.info('Browser waiting until DOM loads...')
   } else {
-    document.addEventListener('DOMContentLoaded', startApprender);
+    document.addEventListener('DOMContentLoaded', startAppRender);
   }
 }
