0f01445d4754dcf7b4904c62d98004b8be82de3d
Roman Shalman <romanshalman@gmail.com>
2020-10-03 15:07:01 +0300

    fix: refactor tickets_close_interceptor app

--- Freshworks-Samples/Freshdesk/tickets_close_interceptor/app/app.js
+++ Freshworks-Samples/Freshdesk/tickets_close_interceptor/app/app.js
@@ -5,79 +5,81 @@
  * any timer running. If so, it rejects the close action and displays an
  * error message.
  */
-// Function executed when the ticked is getting closed.
-var timerValidation = function timerValidation(event) {
-  client.data.get("ticket").then(
-    function (ticketData) {
-      var baseUrl = "https://<%= iparam.freshdesk_domain %>.freshdesk.com/";
-      var url = baseUrl.concat(ticketData.ticket.id, "/time_entries");
-      var options = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Basic <%= encode(iparam.freshdesk_key + ':x') %>"
-        }
-      };
 
-      client.request.get(url, options).then(
-        function (data) {
-          if (data.status === 200) {
-            var response = JSON.parse(data.response);
-            var runningTimer = response.filter(function (timeEntry) {
-              return timeEntry.timer_running === true;
-            });
+$(document).ready(appInitialized);
 
-            if (runningTimer.length > 0) {
-              event.helper.fail(
-                "Timer(s) running. Stop the timer(s) before proceeding to close the ticket."
-              );
-            } else {
-              event.helper.done();
-            }
-          } else {
-            handleErrors(data, event);
-          }
-        },
-        function (e) {
-          handleErrors(e, event);
-        }
-      );
-    },
-    function (e) {
-      handleErrors(e, event);
-    }
-  );
-};
-
-var handleErrors = function handleErrors(e, event) {
-  event.helper.fail(
-    "Some Error occured in validating ticket close. Status: "
-      .concat(e.status, " ")
-      .concat(e.message)
-  );
-};
-
-var propertiesUpdatedCallback = function propertiesUpdatedCallback(event) {
-  // Use event.helper.getData() to get the event details.
-  var data = event.helper.getData(); //Sample data: { changedAttributes: { status: { old:1, new:2 } } }
-  // status changed to closed
-
-  if (data.changedAttributes.status && data.changedAttributes.status[1] === 5) {
-    timerValidation(event);
-  } else {
-    event.helper.done();
-  }
-};
-
-$(document).ready(function () {
-  app.initialized().then(function (client) {
-    window.client = client;
-    client.events.on("app.activated", function () {
-      client.events.on("ticket.closeTicketClick", timerValidation, {
-        intercept: true
-      });
-      client.events.on("ticket.propertiesUpdated", propertiesUpdatedCallback, {
-        intercept: true
-      });
+function appInitialized() {
+    app.initialized().then(client => {
+        window.client = client;
+        client.events.on("app.activated", appActivated);
     });
-  });
-});
+}
+
+function appActivated() {
+    client.events.on("ticket.closeTicketClick", timerValidation, {
+        intercept: true
+    });
+    client.events.on("ticket.propertiesUpdated", propertiesUpdatedCallback, {
+        intercept: true
+    });
+}
+
+function propertiesUpdatedCallback(event) {
+    // Use event.helper.getData() to get the event details.
+    const data = event.helper.getData(); //Sample data: { changedAttributes: { status: { old:1, new:2 } } }
+    // status changed to closed
+
+    if (data.changedAttributes.status && data.changedAttributes.status[1] === 5) {
+        timerValidation(event);
+    } else {
+        event.helper.done();
+    }
+}
+
+// Function executed when the ticked is getting closed.
+function timerValidation(event) {
+    client.data.get("ticket").then(ticketData => ticketDataRequest(ticketData, event),
+        e => handleErrors(e, event)
+    );
+};
+
+function ticketDataRequest(ticketData, event) {
+    const {url, options} = requestSettings(ticketData)
+    client.request.get(url, options).then(
+        data => timerDataRequestHandler(data, event),
+        e => handleErrors(e, event)
+    )
+}
+
+function requestSettings(ticketData) {
+    const baseUrl = "https://<%= iparam.freshdesk_domain %>.freshdesk.com/";
+    return {
+        url: `${baseUrl}${ticketData.ticket.id}/time_entries`,
+        options: {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Basic <%= encode(iparam.freshdesk_key + ':x') %>"
+            }
+        }
+    }
+}
+
+function timerDataRequestHandler(data, event) {
+    if (data.status === 200) {
+        const runningTimer = JSON.parse(data.response).some(timeEntry => timeEntry.timer_running === true);
+        const failText = "Timer(s) running. Stop the timer(s) before proceeding to close the ticket."
+
+        runningTimer ?
+            event.helper.fail(failText) :
+            event.helper.done()
+
+    } else {
+        handleErrors(data, event);
+    }
+}
+
+function handleErrors(e, event) {
+    event.helper.fail(
+        `Some Error occured in validating ticket close. Status: ${e.status} ${e.message}`
+    );
+}
