95b62f5a9c67525e2785b7e77bbc7d355c3b81dc
Iyosayi <Iyosa14@gmail.com>
2020-10-05 10:23:16 +0100

    refactor: moved postConfigs, getConfigs and validate functions into iparams.html file

--- Freshworks-Samples/App-Development-Features/Configuration-Features/custom_installation_page_app/config/assets/iparams.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/custom_installation_page_app/config/assets/iparams.js
@@ -1,32 +1,10 @@
 const API_LINK = "https://anapioficeandfire.com/api/books";
 
-function getConfigs(savedConfigs) {
-  if (savedConfigs) {
-    $("#bgc").val(savedConfigs.bgColour);
-    $("#book").val(savedConfigs.book);
-  }
-}
-
-function postConfigs() {
-  return {
-    bgColour: $("#bgc").val(),
-    book: $("#book option:selected").val(),
-  };
-}
-
 function showValidationMessage(inputClass, message) {
   var validationSelector = $(`#${inputClass}-validation`);
   validationSelector.fadeIn();
   validationSelector.text(message);
   validationSelector.fadeOut(1500);
-}
-
-function validate() {
-  if ($("#bgc").val() === "#000000") {
-    showValidationMessage("bgc", "Background colour cannot be black!");
-    return false;
-  }
-  return true;
 }
 
 function getBookFromAPI(callback) {
