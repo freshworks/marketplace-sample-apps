3827ff0b70714c8b446037067f8a0d68935b5434
Edgardo Ramírez <soldiercrp@gmail.com>
2020-10-23 03:36:18 -0500

    UPDATE: replace jquery for plain js startTimer.js, REMOVE: iparam_test_data.json

--- Freshworks-Samples/Freshdesk/timer_on_tap/app/startTimer.js
+++ Freshworks-Samples/Freshdesk/timer_on_tap/app/startTimer.js
@@ -1,48 +1,57 @@
-$(document).ready( function() {
-  jQuery('#startTimer').attr('disabled', false);
-  jQuery('.alert').hide();
-  jQuery('.spinner').show();
-  jQuery('#fields').hide();
+var ready = (callback) => {
+  if (document.readyState != 'loading') callback();
+  else document.addEventListener('DOMContentLoaded', callback);
+}
+
+ready(() => {
+  var spinner = document.querySelector('.spinner');
+  var fields = document.querySelector('#fields');
+
+  document.querySelector('#startTimer').setAttribute('disabled', false);
+  document.querySelectorAll('.alert').forEach(box => { box.style.display = 'none' });
+
+  spinner.style.display = 'block';
+  fields.style.display = 'none';
 
   app.initialized().then(function(_client) {
     window._client = _client;
     var baseUrl = `https://<%= iparam.freshdesk_domain %>.freshdesk.com`;
     var url = `${baseUrl}/api/v2/agents`;
+
+    console.log(url)
     var options = {
       "headers" : {
         "Content-Type": "application/json",
         "Authorization": "Basic <%= encode(iparam.freshdesk_key + ':x') %>"
       }
     };
+
     _client.request.get(url, options)
     .then(function(data) {
       if (data.status === 200) {
         var agentList = JSON.parse(data.response);
         for(var agent in agentList) {
-          jQuery('#agent').append(
-          jQuery('<option/>')
-          .attr('value', agentList[agent].id)
-          .text(agentList[agent].contact.name));
+          const option = document.createElement('option');
+          option.text = agentList[agent].contact.name;
+          option.value = agentList[agent].id;
+
+          document.querySelector('#agent').appendChild(option);
         }
-        jQuery('.spinner').hide();
-        jQuery('#fields').show();
+        spinner.style.display = 'none';
+        fields.style.display = 'block';
       }
     }, function(error) {
-      jQuery('.spinner').hide();
-      jQuery('.alert-danger').show();
+      spinner.style.display = 'none';
+      document.querySelector('.alert-danger').style.display = 'block';
     });
   });
 });
 
-/**
- * Handles upon clicking the start timer icon
- */
-
 function addTimer() {
-  var agent = jQuery('#agent').val();
-  var billable = jQuery('#billable').is(':checked');
-  var note = jQuery('#note').val();
+  var agent = document.querySelector('#agent').value;
+  var billable = document.querySelector('#billable').checked;
+  var note = document.querySelector('#note').value;
   _client.instance.send({ message: { agent, billable, note } });
-  jQuery('.alert-success').show();
-  jQuery('#startTimer').attr('disabled', true);
+  document.querySelector('.alert-success').style.display = 'block';
+  document.querySelector('#startTimer').setAttribute('disabled', true);
 }
