2d3e90e1b1718d50da34a06e56088a6f0ec75eaa
João Jesus <47331628+JoaoJesus94@users.noreply.github.com>
2020-10-27 11:29:19 +0000

    [ISSUE-236] - Remove jquery from quick_links_app (#255)
    * refactor(quick_links_app): remove jquery
    
    re #236
    
    * refactor(quick_links_app): reformat code

--- Demo-Apps/quick_links_app/app/scripts/app.js
+++ Demo-Apps/quick_links_app/app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-$(document).ready(function() {
-  var ticketId = '', userId = '', tickets = [], currentTicketObj = {}, domainName = '';
+document.addEventListener("DOMContentLoaded", function () {
+  let ticketId = '', userId = '', tickets = [], currentTicketObj = {}, domainName = '';
 
   function renderApp() {
     return Promise.all([client.data.get('loggedInUser'), client.data.get('ticket'), client.data.get('domainName')]);
@@ -10,23 +10,27 @@ $(document).ready(function() {
   // Saves the changes to the db
   function setBookmarks() {
     client.db.set("ticket_bookmarks:" + userId, {tickets: tickets})
-    .then(null, function(error) {
-      client.interface.trigger("showNotify", {type: "danger", title: "Error", message: "Changes could not be saved. Please try again."});
-    }).finally(function(){
+      .then(null, function (error) {
+        client.interface.trigger("showNotify", {
+          type: "danger",
+          title: "Error",
+          message: "Changes could not be saved. Please try again."
+        });
+      }).finally(function () {
       displayBookmarks();
     });
   }
 
   // Gets the bookmarks from the db
   function getBookmarks() {
-    return client.db.get("ticket_bookmarks:"+ userId);
+    return client.db.get("ticket_bookmarks:" + userId);
   }
 
   // Picks specific keys from the object
   function pickKeysFromObj(ticketObj) {
-    var arr = ['id', 'subject', 'description_text'];
-    var trimmedObj = {};
-    arr.forEach(function(val){
+    let arr = ['id', 'subject', 'description_text'];
+    let trimmedObj = {};
+    arr.forEach(function (val) {
       trimmedObj[val] = ticketObj[val];
     });
     return trimmedObj;
@@ -34,23 +38,24 @@ $(document).ready(function() {
 
   // Updates the tickets array
   function changeBookmarks(type, ticketObj) {
-    return getBookmarks().then(function(result) {
-      var index = (result.tickets || []).map(function(val){ return val.id;}).indexOf(ticketObj.id);
-      if(type == 'add') {
-        if(index < 0) {
+    getBookmarks().then(function (result) {
+      let index = (result.tickets || []).map(function (ticket) {
+        return ticket.id;
+      }).indexOf(ticketObj.id);
+      if (type === 'add') {
+        if (index < 0) {
           tickets.push(pickKeysFromObj(currentTicketObj));
           setBookmarks();
         }
-      }
-      else if (type == 'remove') {
-        if(index > -1) {
-          tickets.splice(index , 1);
+      } else if (type === 'remove') {
+        if (index > -1) {
+          tickets.splice(index, 1);
           setBookmarks();
         }
       }
-    }, function(error) {
-      if(error.status == 404 && type == 'add') {
-        tickets = [ pickKeysFromObj(currentTicketObj) ];
+    }, function (error) {
+      if (error.status === 404 && type === 'add') {
+        tickets = [pickKeysFromObj(currentTicketObj)];
         setBookmarks();
       }
     });
@@ -58,49 +63,58 @@ $(document).ready(function() {
 
   //Displays the updated the list in the UI
   function displayBookmarks() {
-    $('.bookmarks-ul').html('');
-    var template = '<li class="bookmarks-li"><a href="https://${domainName}/a/tickets/${id}" target="_blank" class="bookmark-link">${subject}</a></li>';
-    tickets.forEach(function(val){
+    let bookmark = document.getElementsByClassName('bookmarks-ul')[0];
+    bookmark.innerHTML = '';
+
+    function setTemplate({id, domainName, subject}) {
+      return `<li class="bookmarks-li"><a href="https://${domainName}/a/tickets/${id}" target="_blank" class="bookmark-link">${subject}</a></li>`;
+    }
+
+    tickets.forEach(function (val) {
       val.domainName = domainName;
-      $.tmpl( template, val).appendTo('.bookmarks-ul');
+      bookmark.innerHTML += setTemplate(val);
     });
 
-    if(tickets.find(function(val){ return val.id == ticketId;})){
-      $('#add_to_bookmarks').prop('disabled', true);
-    }
-    else{
-      $('#add_to_bookmarks').prop('disabled', false);
+    if (tickets.find(function (val) {
+      return val.id === ticketId;
+    })) {
+      document.getElementById('add_to_bookmarks').disabled = true;
+    } else {
+      document.getElementById('add_to_bookmarks').disabled = false;
     }
 
-    $('.bookmarks-li').each(function(index, el){
-      $clamp(el, {clamp: 2});
-    });
-  }  
+    let bookmarksList = document.getElementsByClassName('bookmarks-li');
+    if (bookmarksList > 0) {
+      bookmarksList.forEach(function (index, el) {
+        $clamp(el, {clamp: 2});
+      });
+    }
+  }
 
-  app.initialized().then(function(client) {
+  app.initialized().then(function (client) {
     window.client = client;
-    
-    renderApp().then(function(value){
+
+    renderApp().then(function (value) {
       userId = value[0].loggedInUser.id;
       ticketId = value[1].ticket.id;
       domainName = value[2].domainName;
 
       currentTicketObj = value[1].ticket;
-      if(userId && ticketId) {
-        getBookmarks().then (function(records) {
+      if (userId && ticketId) {
+        getBookmarks().then(function (records) {
           tickets = records.tickets || [];
           displayBookmarks();
         });
       }
     });
-    
-    $('#add_to_bookmarks').on('click', function() {
-      if(userId && ticketId) {
+
+    document.getElementById('add_to_bookmarks').addEventListener('click', function () {
+      if (userId && ticketId) {
         changeBookmarks('add', currentTicketObj);
       }
     });
-    
-    $('#manage_bookmarks').on('click', function() {
+
+    document.getElementById('manage_bookmarks').addEventListener('click', function () {
       client.interface.trigger("showModal", {
         title: "Manage Bookmarks",
         template: "views/modal.html",
@@ -110,12 +124,12 @@ $(document).ready(function() {
         }
       });
     });
-    
+
     client.instance.receive(
-      function(event)  {
-        var data = event.helper.getData();
-        if(data.message.type == 'removeTicket') {
-          changeBookmarks('remove', { id: data.message.ticketId });
+      function (event) {
+        let data = event.helper.getData();
+        if (data.message.type === 'removeTicket') {
+          changeBookmarks('remove', {id: data.message.ticketId});
         }
       }
     );
