546bcb51f24bd2268b99880bad81b70e0bace1b8
Amaresh  S M <30730124+amareshsm@users.noreply.github.com>
2020-10-28 20:41:44 +0530

    Sample App to demostrate Authentication (Freshchat)
    This is sample app to demonstrate Freshchat authentication. It authenticates the user and displays the list of all agents configured by the business that uses Freshchat.

---
+++ Freshworks-Samples/Freshchat/authentication-demonstration/app/dialog.js
@@ -0,0 +1,37 @@
+/**
+ * shows all the Agents in the UI
+ * @param {*} agents 
+ */
+let showAgents = (agents) => {
+    let table = document.getElementsByTagName('table')[0].innerHTML;
+    agents.forEach(function (item) {
+        let groups = "";
+        item['groups'].forEach(function (grp) {
+            groups = groups + grp + ',';
+        });
+        table = table + `<tr>
+        <td>${item['first_name']}</td>
+        <td>${item['email']}</td>
+        <td>${item['role_name']}</td>
+        <td>${groups.trim().length > 0 ? groups : '-'}</td>
+      </tr>`;
+    });
+    document.getElementsByTagName('table')[0].innerHTML = table;
+}
+
+app.initialized()
+    .then(function (_client) {
+        window.client = _client;
+        //Retrieves the data within the Agents dialog box
+        client.instance.context().then(function (context) {
+            //On success displays all the agents
+            showAgents(context.data["agents"]);
+        }).catch(function (error) {
+            //On failure notifies the error message to the user
+            client.interface.trigger("showNotify", {
+                type: "danger",
+                message: "Error occured while retriving data"
+            });
+            console.log(error);
+        });
+    });
