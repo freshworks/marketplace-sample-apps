10e6d7e3dbedc3916223d27ffcbc9fa07ba57081
Amaresh  S M <30730124+amareshsm@users.noreply.github.com>
2020-11-02 10:19:22 +0530

    Sample App to demonstrate Authentication (#270)
    * Sample App to demostrate Authentication (Freshchat)
    
    This is sample app to demonstrate Freshchat authentication. It authenticates the user and displays the list of all agents configured by the business that uses Freshchat.
    
    * Sample App to demonstrate Authentication (Freshchat)
    
    This is sample app to demonstrate Freshchat authentication. It authenticates the user and displays the list of all agents configured by the business that uses Freshchat.
    
    * Added screenshots and updated styles
    
    * Removed jQuery and other dependencies that relied on jQuery  (#269)
    
    * Removed jQuery and other dependencies that relied on jQuery such as Boostrap (previously used to align one element), and the select2 library which also had jquery as a dependency but was not used anywhere in the project. Converted relevant jQuery code to Vanilla ES5 javascript equivalents.
    
    * Added in newline characters at the end of js and css files
    
    * IDE formatter seemed to be deleting my newline char at the end of the files, fixed now
    
    * Sample App to demostrate Authentication (Freshchat)
    
    This is sample app to demonstrate Freshchat authentication. It authenticates the user and displays the list of all agents configured by the business that uses Freshchat.
    
    * Sample App to demonstrate Authentication (Freshchat)
    
    This is sample app to demonstrate Freshchat authentication. It authenticates the user and displays the list of all agents configured by the business that uses Freshchat.
    
    * Added screenshots and updated styles
    
    * revised code
    
    Co-authored-by: Ethan Soo Hon <ethanny2@gmail.com>

---
+++ Freshworks-Samples/Freshchat/authentication-demonstration/app/app.js
@@ -0,0 +1,85 @@
+/*
+ * Sample code to demonstrate Authentication for Freshchat
+ * 
+ * This is a sample app to demonstrate Freshchat authentication. It authenticates the user and displays the 
+ * list of all agents configured by the business that uses Freshchat.
+ */
+
+/*
+ * Opens and sends the data to Agents dialog once the show all agents button is clicked.
+ * @param {Object} data 
+ */
+let showDialog = (data) => {
+    client.interface.trigger("showDialog", {
+        title: "All Agents",
+        template: "dialog.html",
+        data: data
+    }).then(function (data) {
+        console.info(data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
+
+/*
+ * Register the click handler to list all agents
+ * @param {Object} data
+ */
+let listAllAgents = (data) => {
+    let listAgentsBtn = document.getElementById('listagents-btn');
+    listAgentsBtn.addEventListener("click", showDialog.bind(null, data));
+}
+
+/* 
+ * Shows error message to user/agent
+ *  @param {Object} error
+ */
+let showError = (error) => {
+    let reponse = JSON.parse(error.response);
+    //Notify the agent/user that something went wrong while fetching the data.
+    client.interface.trigger("showNotify", {
+        type: "danger",
+        message: reponse["errorMessage"] || "Error occured while fetching data"
+    });
+    let listAgentsBtn = document.getElementById('listagents-btn');
+    listAgentsBtn.style.display = "none";
+    let errorMsg = `<img src="error.png" height=35" width="35" /> ${reponse["errorMessage"]} <br>
+    Error occured while fetching data`;
+    document.getElementById('error').innerHTML = errorMsg;
+}
+
+
+app.initialized()
+    .then(function (_client) {
+        window.client = _client;
+        client.events.on('app.activated',
+            function () {
+                let headers = { "Authorization": "Bearer <%= iparam.apiKey %>" };
+                let options = { headers: headers };
+                let url = "https://api.freshchat.com/v2/agents?items_per_page=6";
+                //Calls the agents API and renders the view based on the response
+                client.request.get(url, options)
+                    .then((data) => {
+                        let response = JSON.parse(data.response);
+                        //on success 
+                        listAllAgents(response);
+                    }).catch((error) => {
+                        //on failure 
+                        console.error(error);
+                        showError(error);
+                    });
+                //shows currently logged-in agent.
+                client.data.get('loggedInAgent')
+                    .then(function (data) {
+                        document.getElementById('apptext').innerHTML ="Agent logged in is " + data.loggedInAgent.email;
+                    })
+                    .catch(function (error) {
+                        // Logs and Notifies the user/agent that something went wrong while retrieving logged-in agent
+                        client.interface.trigger("showNotify", {
+                            type: "danger",
+                            message: "Unable to retrieve logged-in agent details"
+                        });
+                        console.error('Unable to retrieve logged-in agent details - ', error);
+                    });
+            });
+    });
