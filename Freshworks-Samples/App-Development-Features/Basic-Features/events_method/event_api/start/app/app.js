e353f0cb4cb05e6b3e23f9f853da534daef2cbae
Raviraj Subramanian <raviraj.subramanian@freshworks.com>
2021-01-21 11:49:24 +0530

    Address minor review comments on the formatting

--- Freshworks-Samples/App-Development-Features/Basic-Features/events_method/event_api/start/app/app.js
+++ Freshworks-Samples/App-Development-Features/Basic-Features/events_method/event_api/start/app/app.js
@@ -1,34 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-	app.initialized()
-		.then(function (_client) {
-			window.client = _client;
-			client.events.on('app.activated', function () {
-				onLoadClickEventHandler();
-			});
-		})
-		.catch(function (error) {
-			console.error(error);
-			showNotification('danger', 'Sorry! Unable to load app');
-		});
+  app.initialized()
+    .then(function (_client) {
+      window.client = _client;
+      client.events.on('app.activated', function () {
+        onLoadClickEventHandler();
+      });
+    })
+    .catch(function (error) {
+      console.error(error);
+      showNotification('danger', 'Sorry! Unable to load app');
+    });
 });
 
 /**
  *   Register click event handler for `Create Ticket` button
  */
 function onLoadClickEventHandler() {
-	document.getElementById('createTicket').addEventListener('click', function () {
-		// Ticket title fetched from user Input
-		var title = document.getElementById('title').value;
-		// Description of the ticket fetched from user input
-		var desc = document.getElementById('desc').value;
-		// Email id of the user, creating the ticket
-		var email = document.getElementById('email').value;
-		if (title && desc && email) {
-			createFreshdeskTicket(title, desc, email);
-		} else {
-			showNotification('danger', 'Ticket Values cannot empty, Fill all values');
-		}
-	});
+  document.getElementById('createTicket').addEventListener('click', function () {
+    // Ticket title fetched from user Input
+    var title = document.getElementById('title').value;
+    // Description of the ticket fetched from user input
+    var desc = document.getElementById('desc').value;
+    // Email id of the user, creating the ticket
+    var email = document.getElementById('email').value;
+    if (title && desc && email) {
+      createFreshdeskTicket(title, desc, email);
+    } else {
+      showNotification('danger', 'Ticket Values cannot empty, Fill all values');
+    }
+  });
 }
 
 // createFreshdeskTicket() function goes here
@@ -40,17 +40,17 @@ function onLoadClickEventHandler() {
  * @param {String} message  	Custom notification message
  */
 function showNotification(status, message) {
-	client.interface.trigger("showNotify", {
-		type: `${status}`,
-		message: `${message}`
-	});
+  client.interface.trigger("showNotify", {
+    type: `${status}`,
+    message: `${message}`
+  });
 }
 
 /**
  * Clear the input fields
  */
 function clearInputfields() {
-	document.getElementById('title').value = '';
-	document.getElementById('desc').value = '';
-	document.getElementById('email').value = '';
+  document.getElementById('title').value = '';
+  document.getElementById('desc').value = '';
+  document.getElementById('email').value = '';
 }
