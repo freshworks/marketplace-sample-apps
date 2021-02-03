2264fda7bf3b317afee862bb6ed5e9171350cdce
Saif Ali Shaik <saifshine7@gmail.com>
2021-02-03 13:06:57 +0530

    👌 IMPROVE: handle api response and render pages

--- Freshworks-Samples/App-Development-Features/Configuration-Features/config-placeholders-freshdesk/app/scripts/app.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-placeholders-freshdesk/app/scripts/app.js
@@ -1,30 +1,26 @@
-var client;
+const JOKE_ENDPOINT = 'https://official-joke-api.appspot.com/random_joke';
 
-document.onreadystatechange = function() {
-  if (document.readyState === 'interactive') renderApp();
+document.onreadystatechange = whenInteractive;
 
-  function renderApp() {
-    var onInit = app.initialized();
+function whenInteractive() {
+  if (document.readyState === 'interactive') {
+    return app.initialized().then(renderJoke).catch(console.error);
+  }
+  function renderJoke(data) {
+    let client = data;
+    let response;
 
-    onInit.then(getClient).catch(handleErr);
+    client.events.on('app.activated', writeJoke);
+    client.request
+      .get(JOKE_ENDPOINT)
+      .then(function (data) {
+        writeJoke(JSON.parse(data.response));
+      })
+      .catch(console.error);
 
-    function getClient(_client) {
-      client = _client;
-      client.events.on('app.activated', onAppActivate);
+    function writeJoke(joke = response) {
+      document.querySelector('.question').textContent = joke.setup;
+      document.querySelector('.answer').textContent = joke.punchline;
     }
   }
-};
-
-function onAppActivate() {
-  var textElement = document.getElementById('apptext');
-  var getContact = client.data.get('contact');
-  getContact.then(showContact).catch(handleErr);
-
-  function showContact(payload) {
-    textElement.innerHTML = `Ticket created by ${payload.contact.name}`;
-  }
-}
-
-function handleErr(err) {
-  console.error(`Error occured. Details:`, err);
 }
