db00f68501e9ba8ffabd072242a4ef01142df77b
Saif Ali Shaik <saifshine7@gmail.com>
2021-02-03 13:27:46 +0530

    🐛 FIX: dont render joke

--- Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-json-fdesk/app/scripts/app.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-json-fdesk/app/scripts/app.js
@@ -11,16 +11,5 @@ function whenInteractive() {
     let response;
 
     client.events.on('app.activated', writeJoke);
-    client.request
-      .get(JOKE_ENDPOINT)
-      .then(function (data) {
-        writeJoke(JSON.parse(data.response));
-      })
-      .catch(console.error);
-
-    function writeJoke(joke = response) {
-      document.querySelector('.question').textContent = joke.setup;
-      document.querySelector('.answer').textContent = joke.punchline;
-    }
   }
 }
