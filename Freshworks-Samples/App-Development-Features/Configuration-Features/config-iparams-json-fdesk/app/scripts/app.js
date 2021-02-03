aec53c01e4622167f1736ea4d85c1c1feda3585d
Saif Ali Shaik <saifshine7@gmail.com>
2021-02-03 15:44:58 +0530

    👌 IMPROVE: app running in background

--- Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-json-fdesk/app/scripts/app.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-json-fdesk/app/scripts/app.js
@@ -1,15 +1,15 @@
-const JOKE_ENDPOINT = 'https://official-joke-api.appspot.com/random_joke';
-
 document.onreadystatechange = whenInteractive;
 
 function whenInteractive() {
   if (document.readyState === 'interactive') {
-    return app.initialized().then(renderJoke).catch(console.error);
+    return app.initialized().then(getClientAPI).catch(console.error);
   }
-  function renderJoke(data) {
-    let client = data;
-    let response;
+}
 
-    client.events.on('app.activated', writeJoke);
-  }
+function getClientAPI(_client) {
+  let client = _client;
+  // For apps those run in background,the callback is invoked everytime page is opened by the Freshdesk Agent.
+  client.events.on('app.activated', function log() {
+    return console.log(`app is activated 🥁`);
+  });
 }
