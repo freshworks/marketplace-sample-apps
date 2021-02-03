91f52498cde1e53f7864e655fd6294e7090079fb
Saif Ali Shaik <saifshine7@gmail.com>
2021-02-03 12:59:13 +0530

    📦 NEW: initialize app with template

---
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-placeholders-freshdesk/app/scripts/app.js
@@ -0,0 +1,30 @@
+var client;
+
+document.onreadystatechange = function() {
+  if (document.readyState === 'interactive') renderApp();
+
+  function renderApp() {
+    var onInit = app.initialized();
+
+    onInit.then(getClient).catch(handleErr);
+
+    function getClient(_client) {
+      client = _client;
+      client.events.on('app.activated', onAppActivate);
+    }
+  }
+};
+
+function onAppActivate() {
+  var textElement = document.getElementById('apptext');
+  var getContact = client.data.get('contact');
+  getContact.then(showContact).catch(handleErr);
+
+  function showContact(payload) {
+    textElement.innerHTML = `Ticket created by ${payload.contact.name}`;
+  }
+}
+
+function handleErr(err) {
+  console.error(`Error occured. Details:`, err);
+}
