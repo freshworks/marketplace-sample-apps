141d480a488d316d420c6dc5c1108775e193011a
Saif Ali Shaik <saifshine7@gmail.com>
2021-02-08 13:38:01 +0530

    📦 NEW: havi api call to be made from iparams json

--- Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-json-fdesk/app/scripts/app.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-json-fdesk/app/scripts/app.js
@@ -1,3 +1,6 @@
+const APIKEY = '2586e690-54ce-43e1-a934-692cbe5c6a97';
+const URL = 'https://api.thecatapi.com/v1/images/search';
+
 document.onreadystatechange = whenInteractive;
 
 function whenInteractive() {
@@ -7,9 +10,22 @@ function whenInteractive() {
 }
 
 function getClientAPI(_client) {
-  let client = _client;
+  window.client = _client;
   // For apps those run in background,the callback is invoked everytime page is opened by the Freshdesk Agent.
-  client.events.on('app.activated', function log() {
-    return console.log(`app is activated 🥁`);
-  });
+  client.events.on('app.activated', makeAPIcall);
+}
+
+function makeAPIcall() {
+  var options = {
+    headers: {
+      'x-api-key': APIKEY
+    }
+  };
+
+  client.request
+    .get(URL, options)
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(console.error);
 }
