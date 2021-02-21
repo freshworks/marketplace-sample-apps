1475efce2dc677abcabd7d55f1364f292726bbf9
Saif Ali Shaik <saifshine7@gmail.com>
2021-02-21 11:53:59 +0530

    👌 IMPROVE:  dropdown label updates

--- Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparam-html-freshdesk/config/assets/iparams.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparam-html-freshdesk/config/assets/iparams.js
@@ -1,1 +1,6 @@
+const dropdown = document.querySelector('.select-alien');
+const datepicker = document.querySelector('.datepicker');
 
+dropdown.addEventListener('fwOptionClick', function updLablOfDrpdwn() {
+  return dropdown.setAttribute('label', dropdown.value);
+});
