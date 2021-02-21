31550b68de19cd79d72793b346ae2b3480013931
Saif Ali Shaik <saifshine7@gmail.com>
2021-02-21 12:54:04 +0530

    👌 IMPROVE: get client

--- Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparam-html-freshdesk/config/assets/iparams.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparam-html-freshdesk/config/assets/iparams.js
@@ -1,6 +1,20 @@
 const dropdown = document.querySelector('.select-alien');
 const datepicker = document.querySelector('.datepicker');
 
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') renderApp();
+  async function renderApp() {
+    try {
+      client = await app.initialized();
+      console.log(client);
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+};
+
 dropdown.addEventListener('fwOptionClick', function updLablOfDrpdwn() {
   return dropdown.setAttribute('label', dropdown.value);
 });
+
+
