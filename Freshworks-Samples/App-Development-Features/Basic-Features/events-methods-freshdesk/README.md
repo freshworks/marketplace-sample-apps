87f27b9e56e53fbc917eab8ee477030e48d343e2
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-05 19:00:21 +0530

    📦 NEW: events method sample app

---
+++ Freshworks-Samples/App-Development-Features/Basic-Features/events-methods-freshdesk/README.md
@@ -0,0 +1,38 @@
+# Events Methods for freshdesk
+
+### Description:
+
+You can observe events that occur in the Freshdesk UI and register a callback with events methods.
+client.events.on("<argument>", callback[,options]) - takes in a callback which will be invoked when desired event occured.
+Events method can observe 3 types of events
+1. Click Events
+2. Change Events
+3. Intercept Events
+
+
+### Screenshots:
+
+#### Click Events
+
+![config page in local development](./screenshots/events%20method%20-%20phone%20number%20on%20click.png)
+
+### Intercept Events
+
+![make secure api calls and get api data](./screenshots/events%20methods%20-%20intercept.png)
+
+### Change Events
+![](./screenshots/events%20methods%20-%20changes%20events.png)
+
+### Prerequisites:
+
+1. Make sure you have a trial Freshdesk account created. You can always [sign up](https://freshdesk.com/signup)
+2. Ensure that you have the [Freshworks CLI](https://community.developers.freshworks.com/t/what-are-the-prerequisites-to-install-the-freshworks-cli/234) installed properly.
+3. Get [Freshdesk API key](https://support.freshdesk.com/support/solutions/articles/215517). After you install the app, you'd notice contacts are being rendered in `ticket_conversation_editor` placeholder.
+
+### Procedure to run the app:
+
+```sh
+# Run the app
+> fdk run
+# app runs on localhost:10001 and sample config page is rendered on /custom_configs
+```
---
+++ Freshworks-Samples/App-Development-Features/Basic-Features/events-methods-freshdesk/app/scripts/tkt_req_info.js
@@ -0,0 +1,66 @@
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') renderApp();
+};
+
+function renderApp() {
+  app
+    .initialized()
+    .then((_client) => {
+      window['client'] = _client;
+      client.events.on('app.activated', eventsInTktDetailsPage);
+    })
+    .catch(console.error);
+}
+
+function eventsInTktDetailsPage() {
+  const spotlight = document.querySelector('.spotlight');
+
+  let clickEvents = [
+    'ticket.replyClick',
+    'ticket.sendReply',
+    'ticket.forwardClick',
+    'ticket.conversationForward',
+    'ticket.forward',
+    'ticket.notesClick',
+    'ticket.addNote',
+    'ticket.closeTicketClick',
+    'ticket.deleteTicketClick',
+    'ticket.previousTicketClick',
+    'ticket.nextTicketClick',
+    'ticket.startTimer',
+    'ticket.stopTimer',
+    'ticket.updateTimer',
+    'ticket.deleteTimer'
+  ];
+
+  let interceptEvents = {
+    prevent: ['ticket.closeTicketClick', 'ticket.deleteTicketClick'],
+    allow: ['ticket.propertiesUpdated', 'ticket.sendReply']
+  };
+
+  clickEvents.forEach(function register(click) {
+    client.events.on(click, function writeToDOM(event) {
+      spotlight.insertAdjacentHTML('afterend', `<fw-label value="${click}" color="green"></fw-label>`);
+    });
+  });
+
+  interceptEvents['prevent'].forEach(function registerCb(click) {
+    client.events.on(click, preventClickEvent, { intercept: true });
+
+    function preventClickEvent(event) {
+      let eventName = event.type;
+      const row = `<fw-label value="${eventName.slice(7)} prevented" color="red"></fw-label>`;
+      spotlight.insertAdjacentHTML('afterend', row);
+    }
+  });
+
+  interceptEvents['allow'].forEach(function registerCb(click) {
+    client.events.on(click, allowClickEvents);
+
+    function allowClickEvents(event) {
+      let eventName = event.type;
+      const row = `<fw-label value="${eventName.slice(7)} allowed" color="red"></fw-label>`;
+      spotlight.insertAdjacentHTML('afterend', row);
+    }
+  });
+}
