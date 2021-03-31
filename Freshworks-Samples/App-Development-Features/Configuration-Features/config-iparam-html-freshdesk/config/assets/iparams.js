41754ceda2dd18800998e65196b8787aa7c29af0
Shaik Saif Ali <saifali.shaik@freshworks.com>
2021-03-31 14:12:43 +0530

    freshdesk: sample app code illustrating iparams.html (#291)
    * 📦 NEW: create template folder
    
    * 📦 NEW: setup all app locations
    
    * 📦 NEW: create helicopter and aeroplane svg
    
    * 🐛 FIX: remove unnecessary icon.svg
    
    * 📦 NEW: use flex and layout index.html
    
    * 📦 NEW: render jokes
    
    * 👌 IMPROVE: each location view
    
    * 🐛 FIX: add client to window to avoid warnings
    
    * 🐛 FIX: ignore dist
    
    * 🐛 FIX: mention loading by dom elements are loaded first time
    
    * 👌 IMPROVE: bridge frontend
    
    * 🐛 FIX: remove non-requried placeholder mentions
    
    * 👌 IMPROVE: iparams.html boilerplate
    
    * 👌 IMPROVE: 3 fields
    
    * 📖 DOC: description and comments
    
    * 📖 DOC: screenshots
    
    * 👌 IMPROVE: have flex props applied
    
    * 👌 IMPROVE: grid to layout
    
    * 🐛 FIX: unnecesary section
    
    * 🐛 FIX: unnecesary section
    
    * 🐛 FIX: labelling bugs
    
    * 👌 IMPROVE:  dropdown label updates
    
    * 👌 IMPROVE: get client
    
    * 🐛 FIX: fresh client missing
    
    * 👌 IMPROVE: validate function in wip
    
    * 👌 IMPROVE: manage inputs
    
    * 🐛 FIX: irrespective of api key app works

---
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparam-html-freshdesk/config/assets/iparams.js
@@ -0,0 +1,18 @@
+const dropdown = document.querySelector('.select-alien');
+const datepicker = document.querySelector('.datepicker');
+
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') renderApp();
+  async function renderApp() {
+    try {
+      var client = await app.initialized();
+      window.client = client;
+    } catch (error) {
+      return console.error(error);
+    }
+  }
+};
+
+dropdown.addEventListener('fwOptionClick', function updLablOfDrpdwn() {
+  return dropdown.setAttribute('label', dropdown.value);
+});
