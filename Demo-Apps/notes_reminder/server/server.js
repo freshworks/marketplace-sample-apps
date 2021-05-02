c8fc12973b591330f85c26d426dffc00553855fa
Saif Ali Shaik <saifshine7@gmail.com>
2021-05-02 18:12:44 +0530

    🐛 FIX: remove comma

--- Demo-Apps/notes_reminder/server/server.js
+++ Demo-Apps/notes_reminder/server/server.js
@@ -1,22 +1,23 @@
 function queueMessage(userId, notes) {
-  $db.set(`${userId}_notifications`, {
+  $db
+    .set(`${userId}_notifications`, {
       notes: notes,
     })
     .then(
       function () {
-        console.log('Queued note to notify');
+        console.log("Queued note to notify");
       },
       function (err) {
-        console.log('Error while pushing note to notify');
-      },
+        console.log("Error while pushing note to notify");
+      }
     );
 }
 
 exports = {
-  events: [{event: 'onScheduledEvent', callback: 'onScheduledEventHandler'}],
+  events: [{ event: "onScheduledEvent", callback: "onScheduledEventHandler" }],
 
   onScheduledEventHandler: function (args) {
-    console.log('onScheduledEventHandler fired 🎇');
+    console.log("onScheduledEventHandler fired 🎇");
     var userId = args.data.userId;
     var note = args.data.note;
     /**
@@ -31,12 +32,13 @@ exports = {
         if (err.status === 404) {
           queueMessage(userId, [args.data.note]);
         }
-      },
+      }
     );
   },
 
   createSchedule: function (scheduleObject) {
-    $schedule.create({
+    $schedule
+      .create({
         name: String(scheduleObject.scheduleName),
         data: {
           userId: scheduleObject.userId,
@@ -51,7 +53,7 @@ exports = {
         },
         function operationErr(err) {
           renderData({
-            message: 'Schedule creation failed',
+            message: "Schedule creation failed",
           });
         }
       );
