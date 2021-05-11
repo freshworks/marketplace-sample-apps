ff7f9e6bc36f3ee983b79b91bc48056a5d6a5f81
teja-kummarikuntla <teja.kummarikuntla@freshworks.com>
2021-05-11 17:51:03 +0530

    Tweak: Joke component at full_page

--- Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-html-freshservice/app/scripts/app.js
+++ Freshworks-Samples/App-Development-Features/Configuration-Features/config-iparams-html-freshservice/app/scripts/app.js
@@ -1,40 +1,55 @@
-const JOKE_ENDPOINT = 'https://official-joke-api.appspot.com/random_joke';
+const JOKE_ENDPOINT = "https://official-joke-api.appspot.com/random_joke";
 
 function getJoke() {
-  client.request.get(JOKE_ENDPOINT).then(
-    function (data) {
-      let setup = JSON.parse(data.response).setup;
-      punchline = JSON.parse(data.response).punchline;
-      document.getElementById('setup').innerHTML = `<fw-label value="Question:" color="red"></fw-label> ${setup}`;
-    }), function(error) {
-      console.log(error )
-    }
+  client.request.get(JOKE_ENDPOINT).then(function (data) {
+    showSpinner(data);
+    let setup = JSON.parse(data.response).setup;
+    punchline = JSON.parse(data.response).punchline;
+    q(".card").style.display = "block";
+    q(
+      "#setup"
+    ).innerHTML = `<fw-label value="Question:" color="red"></fw-label> ${setup}`;
+  }),
+    function (error) {
+      console.log(error);
+    };
+}
+
+function showSpinner(data) {
+  if (data) {
+    q(".spinner-div").style.display = "none";
+  }
 }
 
 function addListner() {
-  document.getElementById('punchline-btn').addEventListener('click', function() {
-    document.getElementById('punchline').innerHTML = `<fw-label value="${punchline}" color="green"></fw-label>`
-  })
+  q("#punchline-btn").addEventListener("click", function () {
+    q(
+      "#punchline"
+    ).innerHTML = `<fw-label value="${punchline}" color="green"></fw-label>`;
+  });
 }
 
 document.onreadystatechange = function () {
-  if (document.readyState === 'interactive') renderApp();
+  if (document.readyState === "interactive") renderApp();
 
   function renderApp() {
     var onInit = app.initialized();
-
     onInit.then(getClient).catch(handleErr);
 
     function getClient(_client) {
       window.client = _client;
-      client.events.on('app.activated', onAppActivate);
+      client.events.on("app.activated", onAppActivate);
     }
   }
 };
 
+function q(selector) {
+  return document.querySelector(selector);
+}
+
 function onAppActivate() {
-  var textElement = document.getElementById('apptext');
-  var getContact = client.data.get('contact');
+  var textElement = q("#apptext");
+  var getContact = client.data.get("contact");
   getContact.then(showContact).catch(handleErr);
 
   function showContact(payload) {
