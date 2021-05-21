6ff198e4da5bd009fed444b70ceaf349e39f18b5
Ganesh Satpute <ganesh.satpute@freshworks.com>
2021-05-21 13:05:04 +0530

    Bookmark Tickets FreshDesk App (#326)
    * Created first draft of agent tickets
    
    * Incorporated review comments.
    
    * Corrected the log levels
    
    * Renamed the directory
    
    * Removed installation steps from README
    
    * Corrected the name
    
    * Followed WebStorm Clean Code suggestions
    
    * Reformatted the code

---
+++ Freshworks-Samples/Freshdesk/bookmark_tickets/README.md
@@ -0,0 +1,41 @@
+# Freshdesk Bookmark Tickets App
+
+## Description 
+This app allows an agent to bookmark tickets to visit later.
+
+## App Demo 
+
+![App Demo](demo.gif)
+
+## Prerequisites:
+* Make sure you have a trial Freshdesk account created
+* Ensure that you have the FreshWorks Developer Kit (FDK) installed properly.
+
+## How to run the app 
+* Go to parent directory of this app 
+* Run `fdk run` command 
+* Visit tickets page https://<your account name>.freshdesk.com/a/tickets/
+  Remember to replace your account number.
+* Choose any ticket and open it. Append the `?dev=true` in the URL and press enter.
+* You'll see an app as shown in App Demo 
+
+## Development Platform Features used in this app
+
+| Feature | Purpose | 
+| --- | --- | 
+| App lifecycle methods | To load current agent tickets on the loading of app | 
+| Serverless app | Data operations are performed on server side instead of client side for persistent store |
+| Installation Parameters | We ask the user the FreshWorks domain prefix so that the ticket URL can be formed properly | 
+| Data Storage & Storage API | To store/retrieve/delete the tickets of agent |
+| Data Method | 1. To get current logged in user details <br/>2.  To get current domain <br/> 3. To get current ticket details | 
+| Server Method Invocation | Calling server side to store/retrieve/delete data |
+| Instance Method | To resize the app size |
+
+
+### Few problems
+* The ticket link could be opened in new window (to provide better UX), but it fails with below error
+    ```
+    Unsafe attempt to initiate navigation for frame with origin 'https://newaccount1619511084587.freshdesk.com' from frame with URL 'http://localhost:10001/iframe/index.html?appId=123456789'. The frame attempting navigation of the top-level window is sandboxed, but the flag of 'allow-top-navigation' or 'allow-top-navigation-by-user-activation' is not set.
+    ```
+  so for now, I'm opening tickets in a new tab.
+* As of now, app widget cannot be resized. So the column "remove ticket" looks bit weird.  
---
+++ Freshworks-Samples/Freshdesk/bookmark_tickets/app/scripts/app.js
@@ -0,0 +1,158 @@
+document.onreadystatechange = function () {
+    if (document.readyState === 'interactive') renderApp();
+
+    function renderApp() {
+        const onInit = app.initialized();
+
+        onInit.then(getClient).catch(handleErr);
+
+        function getClient(_client) {
+            window.client = _client;
+            client.events.on('app.activated', onAppActivate);
+
+            client.instance.resize({height: "500px"});
+        }
+    }
+};
+
+function onAppActivate() {
+    showBookmarkedTickets();
+}
+
+function showBookmarkedTickets() {
+    client.data.get("domainName").then(
+        function (freshdeskDomain) {
+            console.log('Freshdesk domain is ' + freshdeskDomain);
+            getLoggedInUser().then(
+                function (loggedInUser) {
+                    let data = {
+                        'agentId': loggedInUser.id
+                    };
+                    console.log("Getting the bookmarked tickets for agent id " + loggedInUser.id);
+
+                    client.request.invoke('getBookmarkedTickets', data)
+                        .then(
+                            function (response) {
+                                console.log('Tickets received from the backend are');
+                                console.log(response.response.savedTickets);
+
+                                const agentBookmarkedTicketsTable = document.getElementById('agentBookmarkedTicketsTable');
+
+                                // Don't show ticket table if there are no tickets to display
+                                if (response.response.savedTickets == null || response.response.savedTickets.length === 0) {
+                                    agentBookmarkedTicketsTable.style.display = "none";
+                                    return;
+                                }
+
+                                // As there are tickets to be displayed, make the table visible
+                                agentBookmarkedTicketsTable.style.display = "table";
+
+                                // Clear existing rows except headers
+                                const rowCount = agentBookmarkedTicketsTable.rows.length;
+                                for (let x = rowCount - 1; x > 0; x--) {
+                                    agentBookmarkedTicketsTable.deleteRow(x);
+                                }
+
+                                response.response.savedTickets.forEach(ticket => {
+                                    insertRow(freshdeskDomain.domainName, ticket.ticketId, ticket.ticketSubject);
+                                });
+                            },
+                            function (error) {
+                                console.error('Error occurred while getting agent tickets ' + JSON.stringify(error));
+                                document.innerHTML = "Unable to get agent tickets";
+                            }
+                        )
+                });
+        },
+        function (error) {
+            console.error('Unable to get freshworks domain' + JSON.stringify(error));
+            document.innerHTML = "Unable to show the tickets";
+        });
+}
+
+function insertRow(freshdeskDomainName, ticketId, ticketSubject) {
+    const table = document.getElementById('agentBookmarkedTicketsTable').insertRow(1);
+
+    const c1 = table.insertCell(0);
+    const c2 = table.insertCell(1);
+    const c3 = table.insertCell(2);
+
+    c1.innerHTML = '<a target="blank" href="https://' + freshdeskDomainName + '/a/tickets/'
+        + ticketId + '">' + ticketId + '</a>';
+    c2.innerHTML = ticketSubject;
+    c3.innerHTML = '<input id="' + ticketId + '" type="image" src="styles/images/removeRed.svg" style="height:20px; width:20px" onClick="removeTicket(this);"/>';
+}
+
+function bookmarkTicket() {
+    getLoggedInUser().then(function (loggedInUser) {
+        getCurrentTicket().then(function (ticket) {
+            let data = {
+                'agentId': loggedInUser.id,
+                'ticketId': ticket.id,
+                'ticketSubject': ticket.subject
+            };
+
+            console.log('Saving current ticket with data ' + JSON.stringify(data));
+
+            client.request.invoke("bookmarkTicket", data).then(
+                function (data) {
+                    console.info("Successfully saved the agent ticket");
+                    console.log(data);
+
+                    showBookmarkedTickets();
+                },
+                function (err) {
+                    console.error('Error while saving ticket');
+                    console.error(err);
+                });
+        })
+    });
+}
+
+function removeTicket(ticket) {
+    getLoggedInUser().then(function (loggedInUser) {
+        let data = {
+            'agentId': loggedInUser.id,
+            'ticketId': parseInt(ticket.id)
+        }
+        console.info('Removing the agent ticket with data ' + JSON.stringify(data));
+        client.request.invoke("removeAgentsTicket", data).then(
+            function (data) {
+                console.log("Successfully removed the ticket from agent. Response is :");
+                console.log(data);
+
+                showBookmarkedTickets();
+            },
+            function (err) {
+                console.error('Error while saving ticket');
+                console.error(err.message);
+            });
+    });
+}
+
+function getLoggedInUser() {
+    return client.data.get("loggedInUser").then(
+        function (response) {
+            return response.loggedInUser;
+        },
+        function (error) {
+            console.error('Error while getting logged in user' + JSON.stringify(error));
+        }
+    );
+}
+
+function getCurrentTicket() {
+    return client.data.get("ticket").then(
+        function (data) {
+            return data.ticket;
+        },
+        function (error) {
+            // failure operation
+            console.error('Error while fetching ticket details ' + JSON.stringify(error));
+        }
+    );
+}
+
+function handleErr(err) {
+    console.error(`Unable to show agent's saved tickets`, err);
+}
