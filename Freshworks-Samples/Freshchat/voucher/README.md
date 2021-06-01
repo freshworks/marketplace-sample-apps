12a898e50ed8ccf74648700ea5eaa700480edd53
Saif Ali Shaik <saifshine7@gmail.com>
2021-06-01 20:40:35 +0530

    👌 IMPROVE: add whitelisting domain support

--- Freshworks-Samples/Freshchat/voucher/README.md
+++ Freshworks-Samples/Freshchat/voucher/README.md
@@ -8,14 +8,16 @@ Shows the voucher list from an API which can be directly added to the editor are
 
 ![Screenshot](app/demo.png)
 
-Features demonstrated | Notes
--------------------- | ------
- _Feature Name_ | _How the app is using it_
+| Features demonstrated | Notes                     |
+| --------------------- | ------------------------- |
+| Request API        | To make an API call to dummy API github gist|
 
 ### Prerequisites:
-1. Make sure you have the development environment setup with the latest version of FDK. If you are starting from scratch, You can have a look at the  [quick start](https://developers.freshchat.com/v2/docs/quick-start/) mentioned here. Skip this step if you already have the FDK installed
 
-2. Have a sample voucher API which adheres to [this structure](https://gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json). Feel free to modify the code for a different payload structure/API accordingly. You can provide the voucher API endpoint in the installation parameters. Use `config/iparam_test_data.json` for [local testing](https://developers.freshchat.com/v2/docs/quick-start/#test_your_app). You can use `gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json` as a dummy API for local testing purpose
+1. Make sure you have the development environment setup with the latest version of FDK. If you are starting from scratch, You can have a look at the [quick start](https://developers.freshchat.com/v2/docs/quick-start/) mentioned here. Skip this step if you already have the FDK installed
+
+2. Have a sample voucher API which adheres to [this structure](https://gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json). Feel free to modify the code for a different payload structure/API accordingly. You can provide the voucher API endpoint in the installation parameters. You can use `gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json` as a dummy API for local testing purpose. Where the domain to be whitelisted is `gist.githubusercontent.com`.
 
 ### Procedure to run the app:
-2. Have a sample voucher API which adheres to [this structure](https://gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json). Feel free to modify the code for a different payload structure/API accordingly. You can provide the voucher API endpoint in the installation parameters. Use `config/iparam_test_data.json` for [local testing](https://developers.freshchat.com/v2/docs/quick-start/#test_your_app). You can use `gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json` as a dummy API for local testing purpose
+
+1. Have a sample voucher API which adheres to [this structure](https://gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json). Feel free to modify the code for a different payload structure/API accordingly. You can provide the voucher API endpoint in the installation parameters. You can use `gist.githubusercontent.com/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json` as a dummy API for local testing purpose
--- Freshworks-Samples/Freshchat/voucher/app/dialog/dialog.js
+++ Freshworks-Samples/Freshchat/voucher/app/dialog/dialog.js
@@ -1,13 +1,14 @@
 /**
  * Voucher Dialog
  */
-document.addEventListener("DOMContentLoaded", function () {
-  app.initialized()
+document.addEventListener('DOMContentLoaded', function () {
+  app
+    .initialized()
     .then(onAppInitializedCallback)
     .catch(function (error) {
       //Log and notify initialization error
       console.error(error);
-      showNotification("danger", "Unable to initialize the app");
+      showNotification('danger', 'Unable to initialize the app');
     });
 });
 
@@ -17,8 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function onAppInitializedCallback(_client) {
   window.client = _client;
-  client.instance.resize({ height: "500px" });
-  client.request.get("https://<%= iparam.voucher_api %>", {})
+  client.instance.resize({ height: '500px' });
+  client.request
+    .get(
+      'https://<%= iparam.voucher_api %>/hemchander23/d762eaa5beab373218bb61ae6294afda/raw/dummyVoucherData.json',
+      {}
+    )
     .then(function (data) {
       // If we successfully get the voucher data, we can proceed to showing that in the UI
       renderVoucherView(data.response);
@@ -26,11 +31,11 @@ function onAppInitializedCallback(_client) {
     .catch(function (error) {
       //Log and notify the agent/user
       console.error(error);
-      showNotification("danger", "Unable to get voucher data");
+      showNotification('danger', 'Unable to get voucher data');
       client.instance.close();
-    })
+    });
   //Register the click handler for a voucher item
-  document.querySelector("fc_voucher").addEventListener("click", voucherClickHandler);
+  document.querySelector('fc_voucher').addEventListener('click', voucherClickHandler);
 }
 
 /**
@@ -46,7 +51,7 @@ function renderVoucherView(data) {
   } catch (e) {
     //Log and notify the agent/user
     console.error(e);
-    showNotification("danger", "Unable to render voucher view");
+    showNotification('danger', 'Unable to render voucher view');
     client.instance.close();
   }
 }
@@ -57,20 +62,20 @@ function renderVoucherView(data) {
  */
 function voucherClickHandler(e) {
   var voucher = e.target;
-  var voucher_name = voucher.data('vouchertitle') || "N.A";
-  var voucher_desc = voucher.data('voucherdesc') || "N.A";
+  var voucher_name = voucher.data('vouchertitle') || 'N.A';
+  var voucher_desc = voucher.data('voucherdesc') || 'N.A';
   //Use Interface APIs to set the editor value
   var voucher_message = `Voucher Code :  ${voucher_name}  - ${voucher_desc}`;
-  client.interface.trigger("setValue", { id: "editor", value: voucher_message })
+  client.interface
+    .trigger('setValue', { id: 'editor', value: voucher_message })
     .then(function () {
       client.instance.close();
     })
     .catch(function (error) {
-      console.error("Error occured while setting editor value", error);
+      console.error('Error occured while setting editor value', error);
       client.instance.close();
     });
-};
-
+}
 
 /**
  * Shows notification to the agent
@@ -78,8 +83,8 @@ function voucherClickHandler(e) {
  * @param {string} message
  */
 function showNotification(type, message) {
-  client.interface.trigger("showNotify", {
-    type: type || "alert",
-    message: message || "NA"
+  client.interface.trigger('showNotify', {
+    type: type || 'alert',
+    message: message || 'NA'
   });
 }
