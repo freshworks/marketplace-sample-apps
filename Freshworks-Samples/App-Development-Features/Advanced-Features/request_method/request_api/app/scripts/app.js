ddd1f61d48b614d666356c3cfaad2a2516ebac6a
Hemchander S <hemchander.23@gmail.com>
2021-06-07 18:37:15 +0530

    `request_api` whitelisting: httpbin -> xkcd API

--- Freshworks-Samples/App-Development-Features/Advanced-Features/request_method/request_api/app/scripts/app.js
+++ Freshworks-Samples/App-Development-Features/Advanced-Features/request_method/request_api/app/scripts/app.js
@@ -5,8 +5,15 @@ function startAppRender() {
     .then(function (client) {
       let options = { client: true }
       const displayElement = document.getElementById('apptext');
-      client.request.get("https://httpbin.org/get", options).then((payload) => {
-        displayElement.innerHTML = `httpbin responded with <code>${payload.response}</code>`
+      client.request.get("https://xkcd.com/info.0.json", options).then((data) => {
+        const payload = JSON.parse(data.response); console.log(payload)
+        displayElement.innerHTML = `<center>
+                                      <a href="${payload.img}" target="_blank">
+                                        <img src="${payload.img}" width="100%"></img><br/>
+                                      </a>  
+                                      <b>${payload.safe_title}</b><br/> 
+                                      <small>(Click the image to see the cartoon)</small>
+                                    </center>`;
       }, (error) => {
         console.error('An error occurred during the request..')
         console.error(error);
