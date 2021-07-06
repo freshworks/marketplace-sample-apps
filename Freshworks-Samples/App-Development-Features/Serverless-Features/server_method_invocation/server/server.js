4e2c0cd7dfc5e50a6f5fd532b7b4461675e10b9a
Hemchander S <hemchander.23@gmail.com>
2021-07-06 15:51:37 +0530

    `server_method_invocation` fix

--- Freshworks-Samples/App-Development-Features/Serverless-Features/server_method_invocation/server/server.js
+++ Freshworks-Samples/App-Development-Features/Serverless-Features/server_method_invocation/server/server.js
@@ -1,15 +1,18 @@
 var request = require('request');
 
 exports = {
+  /* Disclaimer: The use of mock servers in production environment is disallowed. HttpBin.org can be used to debug or test your app's behavior locally.
+     However, this will not work when the app is deployed in a live account. 
+     We recommend switching to a custom mock server controlled by you in such scenarios. */
 
-  makeRequest: function(args) {
+  makeRequest: function (args) {
     request({
       url: 'https://httpbin.org/json',
       method: 'GET',
       headers: {
         Authorization: 'SDFkl2j30sd9iU#'
       }
-    }, function(err, res, body) {
+    }, function (err, res, body) {
       if (err) {
         return renderData({
           message: 'Error while making request'
